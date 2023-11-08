d6eb7d8d25efbcc33af05e430b419f12b6b6234f
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-08 12:17:33 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231108/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 231108/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -7,11 +7,15 @@ while(1):
     if n==1:
         print(arr[n])
         break
+    if n==0:
+        print(-1)
+        break
     if arr[index] == arr[index+1]:
         index +=2
     else:
         print(arr[index])
         break
+    
     if index >= n-1:
         print(-1)
         break
