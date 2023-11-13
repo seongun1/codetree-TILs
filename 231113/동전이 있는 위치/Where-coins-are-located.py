6b68c3d4f215498b0bdfb88de514a4e973ef5f49
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-13 17:25:28 +0900

    Title: 동전이 있는 위치, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231113/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,9 @@
+n,m=map(int,input().split())
+arr= [[0 for i in range(n)] for i in range(n)]
+for i in range (m):
+    x,y= map(int,input().split())
+    arr[x-1][y-1] = 1
+for a in arr:
+    for i in a:
+        print(i,end=' ')
+    print()
