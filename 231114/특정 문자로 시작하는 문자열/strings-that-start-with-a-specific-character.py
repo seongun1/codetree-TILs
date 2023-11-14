db20af085ecddfa1ff71469cc5b3487d1fcd8a32
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-14 12:02:53 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231114/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
+++ 231114/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
@@ -7,4 +7,7 @@ for a in arr:
     if a[0] == tmp:
         cnt +=1
         tmp_len += len(a)
-print(f"{cnt} {(tmp_len / cnt):.2f}")
+if cnt !=0:
+    print(f"{cnt} {(tmp_len / cnt):.2f}")
+else:
+    print(0,0.00)
