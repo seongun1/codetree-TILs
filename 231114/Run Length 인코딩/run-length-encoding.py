26454d7f5efc6640857d483f74c97c9f2a39c254
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-14 22:01:27 +0900

    Title: Run Length 인코딩, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231114/Run Length 인코딩/run-length-encoding.py
+++ 231114/Run Length 인코딩/run-length-encoding.py
@@ -1,13 +1,17 @@
-arr =input()
-tmp =''
-i=0
-while (i != len(arr)-1):
-    cnt=1
-    for j in range(i+1,len(arr)):
-        if arr[i] == arr[j]:
+arr =list(input())
+index =0
+cnt =1
+tmp=''
+while (index != len(arr)-1):
+    cnt =0
+    for j in range(index,len(arr)):
+        if arr[index] == arr[j]:
             cnt +=1
         else:
             break
-    tmp += arr[i] + str(cnt)
-    i=j 
-print(len(tmp),tmp,sep="\n")
+    tmp += arr[index]+str(cnt)
+    index =j
+    if index == len(arr)-1 and arr[index] != arr[index-1]:
+        tmp += arr[index] + '1'
+print(len(tmp))
+print(tmp)
