d777b717ca229a5ecd505983d005ec99745e3321
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-14 12:04:35 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 231114/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
+++ 231114/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
@@ -1,5 +1,5 @@
 n=int(input())
-arr=[input() for _ in range(4)]
+arr=[input() for _ in range(n)]
 tmp= input()
 tmp_len =0
 cnt=0
@@ -7,7 +7,5 @@ for a in arr:
     if a[0] == tmp:
         cnt +=1
         tmp_len += len(a)
-if cnt !=0:
-    print(f"{cnt} {(tmp_len / cnt):.2f}")
-else:
-    print(0,0.00)
+
+print(f"{cnt} {(tmp_len / cnt):.2f}")
