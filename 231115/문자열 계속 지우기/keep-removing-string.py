4f119ff328c5aea6da92c63eba077f1818a56174
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-15 19:01:26 +0900

    Title: 문자열 계속 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231115/문자열 계속 지우기/keep-removing-string.py
@@ -0,0 +1,19 @@
+a=list(input())
+b=list(input())
+t=len(a)
+tmp=''
+i=0
+while (i<len(a)-1):
+    flag = False
+    for j in range(len(b)):
+        if a[i+j] == b[j]:
+            flag = True
+        else:
+            flag = False
+            break
+    if flag:
+        del a[i:i+j+1]
+        i=0
+    else:
+        i+=1
+print(''.join(a))
