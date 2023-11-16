bf5a405946d7c90230a03784d4608ff7197625fb
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-16 21:28:57 +0900

    Title: 미는 횟수, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231116/미는 횟수/number-of-pushes.py
+++ 231116/미는 횟수/number-of-pushes.py
@@ -1,11 +1,12 @@
 a=input()
 b=input()
 cnt =0
-while (cnt <= len(a)):
+while (cnt < len(a)):
     cnt +=1
     a = a[1:] + a[0]
     if a==b:
         break
+    print(cnt)
 if cnt >= len(a):
     cnt = -1
 print(cnt)
