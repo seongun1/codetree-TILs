e8d1c8d9bc1e5dbf09139f3c5f3d180a45f99789
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-17 10:56:12 +0900

    Title: 최대공약수 구하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 231117/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 231117/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,11 +1,7 @@
 n,m =map(int,input().split())
 def gcd (a,b):
-    min_val =0
+    min_val =min(a,b)
     index = 0
-    if a<b:
-        min_val = a
-    else:
-        min_val =b
     for i in range(min_val,0,-1):
         if not a%i and not b%i:
             index = i
