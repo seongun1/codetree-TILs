1e403b0a686e3aac458a420d322931aa32c84179
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-18 21:34:03 +0900

    Title: 강력한 폭발, Time: 752ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 231118/강력한 폭발/strong-explosion.py
@@ -0,0 +1,87 @@
+n = int(input())
+boom_cnt = 0 # 폭탄 개수
+
+arr,ans = [], []
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1:
+            boom_cnt += 1
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def boom(tmp):
+    idx, cnt = 0,0
+    flag = False
+
+    new_arr = [[0] * n for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] == 1:
+                if tmp[idx] == 1:
+                    for h in range(-2,3):
+                        if not in_range(i+h,j):
+                            continue
+                        new_arr[i+h][j] = -1
+
+                elif tmp[idx] == 2:
+                    if in_range(i+1,j):
+                        new_arr[i+1][j] = -1
+                    if in_range(i-1,j):
+                        new_arr[i-1][j] = -1
+                    if in_range(i,j+1):
+                        new_arr[i][j+1] = -1
+                    if in_range(i,j-1):
+                        new_arr[i][j-1] = -1
+                    new_arr[i][j] = -1
+
+                elif tmp[idx] == 3:
+                    if in_range(i-1,j-1):
+                        new_arr[i-1][j-1] = -1
+                    if in_range(i-1,j+1):
+                        new_arr[i-1][j+1] = -1
+                    if in_range(i+1,j-1):
+                        new_arr[i+1][j-1] = -1
+                    if in_range(i+1,j+1):
+                        new_arr[i+1][j+1] = -1
+                    new_arr[i][j] = -1
+                idx += 1
+                if idx == boom_cnt:
+                    flag = True
+                    break
+        if flag:
+            break
+
+    for i in range(n):
+        for j in range(n):
+            if new_arr[i][j] == -1:
+                cnt += 1
+    
+    return cnt
+
+maxcnt = -1
+def print_ans():
+    global maxcnt
+    cnt = boom(ans) # [1, 1] \n [1,2]
+
+    if cnt >= maxcnt:
+        maxcnt = cnt
+    
+
+def choose(cur_num): # cur_num은 해당 자리 위치
+    if cur_num == boom_cnt+1:
+        print_ans()
+        return
+    
+    for i in range(1, 4):  
+        ans.append(i)
+        choose(cur_num + 1)
+        ans.pop()
+    return
+
+choose(1)
+print(maxcnt)
