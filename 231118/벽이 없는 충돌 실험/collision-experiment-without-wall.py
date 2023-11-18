76067da9fa4d561cc495d7d2e8421975f83fbd3d
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-18 21:01:35 +0900

    Title: 벽이 없는 충돌 실험, Time: 8103ms, Memory: 148MB, Status: Time Limit Exceed - CodeTree

--- 231118/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 231118/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -1,6 +1,5 @@
 import sys
-INT_MAX = sys.maxsize
-INT_MIN = -sys.maxsize
+input = sys.stdin.readline
 
 mapper = {
     "U": 0,
@@ -9,22 +8,24 @@ mapper = {
     "L": 3
 }
 
+coords = []
 grid = [[None for _ in range(4001)] for __ in range(4001)]
 OFFSET_1 = 1000
 dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
-coords = []
+
 
 def in_range(x,y):
     return 0<=x<4001 and 0<=y<4001
 
 # 구슬들을 움직이는 함수
 def move():
-    global marbles, coords # [] [] 
+    global marbles, coords
+    
     crash = False
-    coords.clear()
+    coords = []
 
-    for i in range(len(marbles)):
-        idx, x, y, w, d = marbles[i]
+    for i in marbles:
+        idx, x, y, w, d = i
         nx, ny = x + dx[d], y + dy[d]
 
         if not in_range(nx,ny):
@@ -42,41 +43,22 @@ def move():
                 continue
             grid[nx][ny] = [idx, nx, ny, w, d]
 
-    marbles.clear()
+    marbles = []
     for x, y in coords:
         marbles.append(grid[x][y])
         grid[x][y] = None
 
     return crash   
 
-def get_max_min_pos(x, y):
-    global max_x, max_y, min_x, min_y
-
-    if x > max_x : 
-        max_x = x
-    elif x < min_x :
-        min_x = x
-    
-    if y > max_y : 
-        max_y = y
-    elif y < min_y :
-        min_y = y
-
 for _ in range(int(input())):
     N = int(input())
-
-    max_x, max_y = INT_MIN, INT_MIN 
-    min_x, min_y = INT_MAX, INT_MAX
 
     marbles, record = [], -1
     for i in range(1,N+1):
         x,y,w,d = input().split()
         marbles.append( [i,(int(x) + OFFSET_1)*2,(int(y) + OFFSET_1)*2,int(w),mapper[d]] )
-        get_max_min_pos((int(x) + OFFSET_1) * 2, (int(y) + OFFSET_1) * 2)
 
-    max_time = max( (max_x - min_x), (max_y - min_y) )
-
-    for second in range(max_time):
+    for second in range(4001):
         if move():
             record = second+1
 
