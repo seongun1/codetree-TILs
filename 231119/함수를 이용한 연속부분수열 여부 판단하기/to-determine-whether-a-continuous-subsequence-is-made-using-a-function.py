a029c08790b1d36a24805880d49af4bfe4818bb3
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-19 14:32:22 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231119/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 231119/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -2,13 +2,18 @@ a,b=map(int,input().split())
 arr=list(map(int,input().split()))
 brr=list(map(int,input().split()))
 def is_bubun(a,b,an,bn):
-    for i in range(an-bn):
+    for i in range(an-bn+1):
         if a[i] == b[0]:
+            flag =True
             for j in range(bn):
                 if a[i+j] != b[j]:
-                    return False
-            return True
-    return False
+                    flag =False
+                    break
+    if flag:
+        return True
+    else:
+        return False
+
 
 if is_bubun(arr,brr,a,b):
     print('Yes')
