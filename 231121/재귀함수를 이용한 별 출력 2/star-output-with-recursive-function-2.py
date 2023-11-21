f00910730ef79b4cdacf4a2123c8d455e6aadf53
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-21 21:50:40 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 132ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 231121/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
@@ -0,0 +1,12 @@
+n=int(input())
+def star(n):
+    if n==0:
+        return
+    for i in range(n):
+        print('*',end=' ')
+    print()
+    star(n-1)
+    for i in range(n):
+        print('*',end=' ')
+    print()
+star(n)
