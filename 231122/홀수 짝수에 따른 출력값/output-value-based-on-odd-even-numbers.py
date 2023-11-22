2ff7aa1c9e3f45a70f82d911f2132dbd87965e20
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-22 21:11:54 +0900

    Title: 홀수 짝수에 따른 출력값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231122/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -0,0 +1,14 @@
+n=int(input())
+def print_odd(n):
+    if n ==1:
+        return 1
+    return print_odd(n-2) + n
+def print_even(n):
+    if n==0:
+        return 0
+    n -= 2
+    return  print_even(n-2) +n
+if not n%2:
+    print(print_even(n))
+else:
+    print(print_odd(n))
