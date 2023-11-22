00e2ef4618d7e9d99b5e36abc69596580f504e48
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-22 21:14:02 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231122/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ 231122/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -1,13 +1,10 @@
 n=int(input())
-def print_odd(n):
-    if n ==1:
-        return 1
-    return print_odd(n-2) + n
-def print_even(n):
+
+def print_val(n):
     if n==0:
         return 0
-    return  print_even(n-2) +n
-if not n%2:
-    print(print_even(n))
-else:
-    print(print_odd(n))
+    if n==1:
+        return 1
+    return  print_val(n-2) +n
+
+print(print_val(n))
