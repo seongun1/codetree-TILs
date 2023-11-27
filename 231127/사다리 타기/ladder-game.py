eb614a005b8bccd462ddefa02e44dc2139d0a516
김성운 <115334493+seongun1@users.noreply.github.com>
2023-11-27 22:36:07 +0900

    Title: 사다리 타기, Time: 262ms, Memory: 26MB, Status: Passed - CodeTree

--- 231127/사다리 타기/ladder-game.py
+++ 231127/사다리 타기/ladder-game.py
@@ -1,74 +1,43 @@
 n,m = map(int,input().split())
 
-# [ 라인번호, 숫자 ]
-arr = [[i,i] for i in range(1,n+1)] # [[1, 1], [2, 2], [3, 3], [4, 4]]
+line = list()
+select_line = list()
+ans = m
 
-line = []
 for i in range(m):
-    a,b = map(int,input().split())
-    line.append((a,b))
+    a,b = tuple(map(int,input().split()))
+    line.append((b,a-1))
 
-line.sort(key = lambda x : x[1])
-#print(line)
+line.sort()
 
-result = []
+def possible():
+    # num1 은 초기값에 대한 결과
+    num1,num2 = [i for i in range(n)], [i for i in range(n)]
 
-def move(line):
-    res = []
-    for i in range(1,n+1):
-        cur_idx = i
-        height = 0 # 얘도 증가해야함
-
-        for j in line:
-            a,b = j
-
-            if a == cur_idx and height <= b:
-                cur_idx = a+1
-            
-            elif a == cur_idx - 1 and height <= b:
-                cur_idx = a 
-
-            height = b
-        res.insert(cur_idx-1,i)
-    return res
-result = move(line)
-#print(result) # 잘 들어옴
-
-temp_line = []
-ans = []
-min_cnt = 200
-
-def print_ans():
-    global min_cnt
-    ans = move(temp_line)
-
+    # 한번에 결과 찾기
+    for _, idx in line:
+        num1[idx], num1[idx+1] = num1[idx+1], num1[idx]
+    for _, idx in select_line:
+        num2[idx], num2[idx+1] = num2[idx+1], num2[idx]
+    
     for i in range(n):
-        if ans[i] != result[i]:
-            return
+        if num1[i] != num2[i]:
+            return False
+    return True
 
-    #print(ans)
-    dup_cnt = 0
-    for i in range(len(temp_line)-1):
-        for j in range(i+1, len(temp_line)):
-            if (temp_line[i] == temp_line[j]):
-                dup_cnt += 1
-    if min_cnt >= len(temp_line):
-        min_cnt = len(temp_line) - dup_cnt
+def find_min(cnt):
+    global ans
+
+    if cnt == m:
+        if possible():
+            ans = min(ans, len(select_line))
+        return
+    
+    select_line.append(line[cnt])
+    find_min(cnt+1)
+    select_line.pop()
+    find_min(cnt+1)
 
 
-
-def putLine(cur_num):
-    if cur_num == n+1:
-        print_ans()
-        return 
-
-    for i in range(cur_num, len(line)):
-        temp_line.append(line[i])
-        putLine(cur_num + 1)
-        temp_line.pop()
-putLine(1)
-
-if min_cnt == 200:
-    print(0)
-else:
-    print(min_cnt)
+find_min(0)
+print(ans)
