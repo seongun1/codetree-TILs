f0aead614200b3170dca2aff223027777e8396ce
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-04 21:29:41 +0900

    Title: 그 요일은, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 231204/그 요일은/the-day-of-the-day.py
+++ 231204/그 요일은/the-day-of-the-day.py
@@ -1,38 +1,25 @@
-m1,d1,m2,d2=tuple(map(int,input().split()))
-A = str(input())
+import sys
+m1,d1,m2,d2 = map(int,input().split())
+a= input()
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# m1,d1,m2,d2 = 12, 14, 12, 29
-# A = 'Fri'
-
-# m1, d1, m2, d2 = 2, 5, 3, 9
-# A = 'Mon'
-
-# m1, d1, m2, d2 = 12, 11, 12, 20
-# A = 'Thu'
-
-day=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
-idx = 0
-
-for i in range(len(day)) :
-    if A == day[i] :
-        idx = i
-
-def total_days(m,d):
-    nums_of_day =[0,31,29,31,30,31,30,31,31,30,31,30,31]
-    total_days = d
-
-    for i in range(1, m) :
-        total_days += nums_of_day[i]
-
-    return total_days
-
-diff = total_days(m2, d2) - total_days(m1, d1) + 1
-cnt = 0
-week_of_day = 0
-
-for i in range(1, diff + 1) :
-    week_of_day = (week_of_day + 1) % 7
-
-    if day[week_of_day - 1] == A : 
-        cnt += 1
-print(cnt)
+def cnt_date(m1,d1,m2,d2):
+    t1,t2 = 0,0
+    for i in range(1,m1):
+        t1 += num_of_days[i]
+    t1 += d1
+    for i in range(1,m2):
+        t2 += num_of_days[i]
+    t2 += d2
+    return t2 - t1 
+    
+arr=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+total_days = cnt_date(m1,d1,m2,d2) - arr.index(a)
+count =0
+if total_days >= 0:
+    count +=1
+else:
+    print(count)
+    exit(0)
+count += total_days //7
+print(count)
