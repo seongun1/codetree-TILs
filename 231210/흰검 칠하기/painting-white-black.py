58ad1e1fd15b674f1f6388751d68747f2b2f5d35
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-10 12:10:01 +0900

    Title: 흰검 칠하기, Time: 152ms, Memory: 30MB, Status: Passed - CodeTree

--- 231210/흰검 칠하기/painting-white-black.py
+++ 231210/흰검 칠하기/painting-white-black.py
@@ -2,36 +2,31 @@ n=int(input())
 MAX = 100000
 point =MAX
 arr = [0 for _ in range(2*MAX +1)]
-arr_grey = [0 for _ in range(2*MAX +1)]
+arr_white =[0 for _ in range(2*MAX +1)]
+arr_black = [0 for _ in range(2*MAX +1)]
 grey = 0
 for _ in range(n):
     x,dire = input().split()
     x = int(x)
     if dire == 'L':
         for i in range(point,point-x,-1):
-            if arr[i] == -1:
-                continue
-            arr[i] =1
-            arr_grey[i] +=1
-            if arr_grey[i] == 22:
-                grey +=1
-                arr[i] = -1
+            arr[i] = 1
+            arr_white[i] +=1
         point = point - x +1
     elif dire =='R':
         for i in range(point,point+x):
-            if arr[i] == -1:
-                continue
-            arr[i] =10
-            arr_grey[i] += 10
-            if arr_grey[i] == 22:
-                grey +=1
-                arr[i] =-1
+            arr[i] = 2
+            arr_black[i] +=1
         point = point +x -1
 
-white,black= 0,0
-for i in range(len(arr)):
-    if arr[i] ==1:
-        white +=1
-    elif arr[i] ==10:
+black,white = 0,0
+for i in range(len(arr_black)):
+    if arr_white[i] >= 2 and arr_black[i] >= 2:
+        grey +=1
+        arr_black[i] =0
+        arr_white[i] =0
+    elif arr_black[i] >=1 and arr[i] == 2:
         black +=1
+    elif arr_white[i] >= 1 and arr[i] ==1:
+        white +=1
 print(white,black,grey)
