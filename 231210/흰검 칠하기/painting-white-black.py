5a1ddcfbeec5710d4c52030b0966767dfd07f791
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-10 11:52:59 +0900

    Title: 흰검 칠하기, Time: 160ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 231210/흰검 칠하기/painting-white-black.py
+++ 231210/흰검 칠하기/painting-white-black.py
@@ -3,24 +3,33 @@ MAX = 100000
 point =MAX
 arr = [0 for _ in range(2*MAX +1)]
 arr_grey = [0 for _ in range(2*MAX +1)]
+grey = 0
 for _ in range(n):
     x,dire = input().split()
     x = int(x)
     if dire == 'L':
         for i in range(point,point-x,-1):
+            if arr[i] == -1:
+                continue
             arr[i] =1
             arr_grey[i] +=1
+            if arr_grey[i] ==6:
+                grey +=1
+                arr[i] = -1
         point = point - x +1
     elif dire =='R':
         for i in range(point,point+x):
+            if arr[i] == -1:
+                continue
             arr[i] =2
             arr_grey[i] += 2
+            if arr_grey[i] == 6:
+                grey +=1
+                arr[i] =-1
         point = point +x -1
-white,black,grey = 0,0,0
+
+white,black= 0,0
 for i in range(len(arr)):
-    if arr_grey[i] >= 6:
-        grey +=1
-        arr[i] = 0
     if arr[i] ==1:
         white +=1
     elif arr[i] ==2:
