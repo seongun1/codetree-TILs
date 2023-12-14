2bee04a8d8103134de633d23f378bd249ee2ffe9
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-14 21:15:34 +0900

    Title: 연속되는 수 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231214/연속되는 수 3/continuous-number3.py
+++ 231214/연속되는 수 3/continuous-number3.py
@@ -4,10 +4,10 @@ for _ in range(n):
     arr.append(int(input()))
 tmp =[]
 cnt =1
-for i in range(1,n):
-    if arr[i] * arr[i-1] < 0:
-        cnt=1
-    else:
+for i in range(n):
+    if i>=1 and arr[i] * arr[i-1] >0:
         cnt +=1
         tmp.append(cnt)
+    else:
+        cnt =1
 print(max(tmp))
