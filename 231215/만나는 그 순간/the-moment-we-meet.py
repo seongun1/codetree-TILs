d0a8c4b64f1194c1ed0ef11496d2aa259f175866
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-15 16:57:38 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231215/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,31 @@
+n,m = map(int,input().split())
+a= [0 for _ in range(1000)]
+b=[0 for _ in range(1000)]
+
+def cal_range(n,a):
+    start =0 #시작점
+    global second
+    second =1
+    for _ in range(n):
+        d,t =input().split()
+        t=int(t)
+        if d =='R':
+            for i in range(second,t+second):
+                start +=1
+                a[i] = start
+            second +=t
+        elif d =='L':
+            for i in range(second,t+second):
+                start -=1
+                a[i] = start
+            second +=t
+    return a
+a= cal_range(n,a)
+b = cal_range(m,b)
+
+def cal_second():
+    for i in range(1,second):
+        if a[i] == b[i]:
+            return i
+    return -1
+print(cal_second())
