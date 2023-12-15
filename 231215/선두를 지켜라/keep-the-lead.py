5b3f4cacbc7c7d766e630bc6e888ab99a56ad7bb
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-15 17:33:58 +0900

    Title: 선두를 지켜라, Time: 147ms, Memory: 40MB, Status: Wrong Answer - CodeTree

---
+++ 231215/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,23 @@
+n,m = map(int,input().split())
+a=[0] * 1000000
+b=[0] * 1000000
+time =1
+for _ in range(n):
+    v,t = map(int,input().split())
+    for i in range(time,time+t):
+        a[i] = a[i-1] + v
+    time +=t
+time=1
+
+for _ in range(m):
+    v,t = map(int,input().split())
+    for i in range(time,time+t):
+        b[i] = b[i-1] + v
+    time +=t
+cnt =0
+for i in range(1,time):
+    if a[i-1] >= b[i-1] and a[i] < b[i]:
+        cnt +=1
+    elif a[i-1] <= b[i-1] and a[i] > b[i]:
+        cnt +=1
+print(cnt-1)
