2e33c47af706b973dd87f6d5b9a42fd763b9726b
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-27 17:40:32 +0900

    Title: 작은 구슬의 이동, Time: 178ms, Memory: 24MB, Status: Passed - CodeTree

--- 231227/작은 구슬의 이동/small-marble-movement.py
+++ 231227/작은 구슬의 이동/small-marble-movement.py
@@ -1,20 +1,21 @@
 n,t = map(int,input().split())
 arr=[[0]*n for _ in range(n)]
-dx=[0,1,-1,0]
-dy=[-1,0,0,1]
+dx=[-1,0,0,1]
+dy= [0,1,-1,0]
+
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
 
-r,c,d = input().split()
-r=int(r)-1
-c=int(c)-1
+x,y,d = input().split()
+x=int(x)-1
+y=int(y)-1
 mapper={'U':0,'D':3,'R':1,'L':2}
 dire = int(mapper[d])
 
 for i in range(t):
-    nr,nc = r+dy[dire],c+dx[dire]
-    if not in_range(nr,nc):
+    nx,ny = x+dx[dire],y+dy[dire]
+    if not in_range(nx,ny):
         dire = 3- dire
     else:
-        r,c = nr,nc
-print(r+1,c+1)
+        x,y = nx,ny
+print(x+1,y+1)
