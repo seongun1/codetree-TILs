8d1bf1fc1745c36c64861c3263ae2d8d4fd98dfb
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-27 14:35:51 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 171ms, Memory: 24MB, Status: Passed - CodeTree

--- 231227/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 231227/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,3 +1,4 @@
+#n:총 개발자의 수 k:전염병 옮길 수 있는 횟수 p:초기에 병이 걸린 개발자 T:악수를 한 횟수
 n,k,p,T = map(int,input().split())
 arr= [0 for _ in range(n+1)] #개발자들의 전염현황
 arr_chance = [k for _ in range(n+1)] # 개발자들이 감염시킬 수 있는 횟수
@@ -7,6 +8,9 @@ for _ in range(T):
 virus.sort(key = lambda x : x[0])
 arr[p] = 1
 for v in virus: #서로 악수를 하는 경우
+    if arr[v[1]] == 1 and arr[v[2]] ==1: #둘 다 감염됐을 경우
+        arr_chance[v[1]] -= 1
+        arr_chance[v[2]] -= 1
     if arr[v[1]] == 1 and arr_chance[v[1]] > 0: #감염됐고, 감염시킬수 있는 횟수가 0 이상인 경우
         arr[v[2]] =1
         arr_chance[v[1]] -=1
