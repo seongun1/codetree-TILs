bfd43eddc29c71be77fcfa9edacf5f6ce17ec0a5
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-27 15:08:39 +0900

    Title: 선두를 지켜라 3, Time: 218ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 231227/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,39 @@
+n,m = map(int,input().split())
+a=[]
+b=[]
+for _ in range(n):
+    a.append(list(map(int,input().split())))
+for _ in range(m):
+    b.append(list(map(int,input().split())))
+tmp=0 #총 걸린 시간
+for i in a:
+    tmp += i[1]
+arr_a= [0 for i in range(tmp+1)]
+arr_b= [0 for i in range(tmp+1)]
+time_a =1
+for i in range(len(a)):
+    v,t = map(int,a[i])
+    for i in range(time_a,time_a+t):
+        arr_a[i] = arr_a[i-1] +v
+    time_a += t
+time_b = 1
+for i in range(len(b)):
+    v,t = map(int,b[i])
+    for i in range(time_b,time_b+t):
+        arr_b[i] = arr_b[i-1] +v
+    time_b += t
+
+cnt =0
+
+arr=[-1 for i in range(tmp+1)]
+for i in range(1,tmp+1):
+    if arr_a[i] > arr_b[i]:
+        arr[i] = 1
+    elif arr_a[i] < arr_b[i]:
+        arr[i] = 2
+    else:
+        arr[i] = 0
+for i in range(1,tmp+1):
+    if arr[i-1] != arr[i]:
+        cnt +=1
+print(cnt)
