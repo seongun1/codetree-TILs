0564a7fa33d1e52351203295c9ed0b1db2ab671b
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-30 21:37:15 +0900

    Title: 최고의 13위치, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231230/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,7 @@
+n=int(input())
+arr=[list(map(int,input().split())) for _ in range(n)]
+max_num =-1
+for i in range(n):
+    for j in range(n-2):
+        max_num = max(max_num,arr[i][j]+arr[i][j+1]+arr[i][j+2])
+print(max_num)
