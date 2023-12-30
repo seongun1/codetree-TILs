e413efd99a23172f6a124ce38a594ab5e68e0f8a
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-30 21:05:38 +0900

    Title: 모이자, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231230/모이자/gather.py
@@ -0,0 +1,9 @@
+n=int(input())
+arr=list(map(int,input().split()))
+min_num =1001
+for i in range(n):
+    tmp=0
+    for j in range(n):
+        tmp += abs(j-i)*arr[j]
+    min_num = min(tmp,min_num)
+print(min_num)
