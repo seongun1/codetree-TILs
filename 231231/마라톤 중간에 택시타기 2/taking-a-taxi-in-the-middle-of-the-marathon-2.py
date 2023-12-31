28d8b6b29d369e5cd96877e3b6201ad5f686969e
김성운 <115334493+seongun1@users.noreply.github.com>
2023-12-31 23:27:52 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231231/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,23 @@
+import sys
+n=int(input())
+def cal_distance (x1,y1,x2,y2):
+    return abs(x1-x2) + abs(y1-y2)
+
+a=[]
+for _ in range(n):
+    p,q = map(int,input().split())
+    p,q = p+1000,q+1000
+    a.append([p,q])
+min_dis = sys.maxsize
+cnt =1
+while (cnt <len(a)-1):
+    distance =0
+    previous =0
+    for i in range(1,len(a)):
+        if i == cnt:
+            continue
+        distance += cal_distance(a[previous][0],a[previous][1],a[i][0],a[i][1])
+        previous =i
+    min_dis = min(min_dis,distance)
+    cnt +=1
+print(min_dis)
