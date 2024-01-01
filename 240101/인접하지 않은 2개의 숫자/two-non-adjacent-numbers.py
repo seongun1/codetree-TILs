885d38de1dba07789b8d4f7d427431098f9db22f
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-01 15:52:26 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 240101/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
+++ 240101/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -1,11 +1,8 @@
 n=int(input())
 arr=list(map(int,input().split()))
-max_sum =0
 ans =0
 for i in range(n):
-    for j in range(n):
-        if abs(i-j) <=1:
-            continue
-        max_sum = max(arr[i]+arr[j],max_sum)
-    ans = max(ans,max_sum)
+    for j in range(i+2,n):
+        if ans < arr[i] + arr[j]:
+            ans = arr[i]+arr[j]
 print(ans)
