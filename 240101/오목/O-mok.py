4ce67ae5d7535aea0564243c42d718c556c45fda
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-01 18:59:23 +0900

    Title: 오목, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240101/오목/O-mok.py
+++ 240101/오목/O-mok.py
@@ -1,9 +1,9 @@
 arr=[list(map(int,input().split())) for _ in range(19)]
 
-dxs = [0,1,1,1]
-dys = [1,0,1,-1]
+dxs = [1,1,1,-1,-1,-1,0,0]
+dys = [-1,0,1,-1,0,1,-1,1]
 def in_range(x,y):
-    return 0<x<19 and 0<y<19
+    return 0<=x<19 and 0<=y<19
 
 def is_answer():
     for i in range(19):
@@ -12,7 +12,7 @@ def is_answer():
                 for dx,dy in zip(dxs,dys):
                     x,y = i,j
                     cnt=1
-                    while (1):
+                    while(1):
                         nx = x+dx
                         ny = y+dy
                         if not in_range(nx,ny):
@@ -20,8 +20,7 @@ def is_answer():
                         if arr[nx][ny] != arr[x][y]:
                             break
                         cnt +=1
-                        x = nx
-                        y = ny
+                        x,y = nx,ny
                     if cnt ==5:
                         print(arr[i][j])
                         print(i+2*dx+1,j+2*dy+1)
