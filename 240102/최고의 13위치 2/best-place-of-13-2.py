11e256394a38991c1b83aae75991441d7bf1be37
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-02 16:27:32 +0900

    Title: 최고의 13위치 2, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240102/최고의 13위치 2/best-place-of-13-2.py
+++ 240102/최고의 13위치 2/best-place-of-13-2.py
@@ -10,16 +10,19 @@ for i in range(n):
         max_coin = max(coin,max_coin)
         if coin == max_coin:
             #기존의 visit 은 False
-            visit[visit_x][visit_y],visit[visit_x][visit_y+1],visit[visit_x][visit_y+1] =  False,False,False
+            visit[visit_x][visit_y],visit[visit_x][visit_y+1],visit[visit_x][visit_y+2] =  False,False,False
             #새로운 곳을 True 처리
             visit_x = i
             visit_y = j
-            visit[visit_x][visit_y],visit[visit_x][visit_y+1],visit[visit_x][visit_y+1] =  True,True,True
+            visit[visit_x][visit_y],visit[visit_x][visit_y+1],visit[visit_x][visit_y+2] =  True,True,True
         coin =0
+
 max_coin2= 0
 for i in range(n):
+    coin=0
     for j in range(n-2):
-        if not visit[i][j]:
+        if not visit[i][j] and not visit[i][j+1] and not visit[i][j+2]:
             coin = arr[i][j] + arr[i][j+1] + arr[i][j+2]
             max_coin2 =max(coin,max_coin2)
+            coin=0
 print(max_coin+max_coin2)
