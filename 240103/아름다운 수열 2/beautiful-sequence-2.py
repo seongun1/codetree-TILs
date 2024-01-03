fd494a39ce89cd76045c7c348c12ee56c06bcf55
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-03 17:21:57 +0900

    Title: 아름다운 수열 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240103/아름다운 수열 2/beautiful-sequence-2.py
+++ 240103/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,19 +1,19 @@
 n,m =map(int,input().split())
 a=list(map(int,input().split()))
 b = list(map(int,input().split()))
-exits=[True] * m
 ans =0
 for i in range(n-m+1):
     cnt =0
+    exits=[True] * m
+
     for j in range(i,i+m):
         for k in range(m):
             if a[j] == b[k] and exits[k] == True:
                 cnt +=1
                 exits[k] =False
+                break
     if cnt == m:
         ans +=1
-
-    exits=[True] * m
-        
+        print(i)
 
 print(ans)
