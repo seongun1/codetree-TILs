7be6a861a1f268d1d21585e95b35e15b46f8a264
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-03 15:56:36 +0900

    Title: 구간 중 최대 합, Time: 167ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240103/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,9 @@
+n,k =map(int,input().split())
+arr=list(map(int,input().split()))
+ans = 0
+for i in range(n-k+1):
+    tmp =0 
+    for j in range(i,i+k):
+        tmp += arr[j]
+    ans = max(tmp,ans)
+print(ans)
