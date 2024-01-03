7a0f75a4145fe79a2a2659a0472f98ec5931c05a
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-03 16:23:04 +0900

    Title: G or H 3, Time: 489ms, Memory: 25MB, Status: Passed - CodeTree

--- 240103/G or H 3/G-or-H-3.py
+++ 240103/G or H 3/G-or-H-3.py
@@ -1,10 +1,10 @@
 n,k = map(int,input().split())
-arr=['0'] * 10000
+arr=['0'] * 10001
 for _ in range(n):
     a,b = input().split()
     arr[int(a)] = b
 ans =0
-for i in range(1,n-k+2):
+for i in range(1,10001-k):
     tmp=0
     for j in range(i,i+k+1):
         if arr[j] == 'G':
