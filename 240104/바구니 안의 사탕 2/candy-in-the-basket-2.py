679604d1c99cb48ef8bd52d35c0eee5a78ef28d7
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-04 23:35:31 +0900

    Title: 바구니 안의 사탕 2, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240104/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240104/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,13 +1,14 @@
 n,k =map(int,input().split())
 #각 사탕의 개수, 바구니의 좌표
-arr=[0] * 101
+arr=[0] * 10001
 for _ in range(n):
     a,b = map(int,input().split())
     arr[b] += a
 ans =0
-for i in range(k,100-k+1):
+for i in range(k,10000-k+1):
     tmp=0
     for j in range(i-k,i+k+1):
         tmp += arr[j]
     ans = max(ans,tmp)
+
 print(ans)
