d875b221dd7bc05cb326ed514b3e94c973678d63
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-06 21:03:27 +0900

    Title: 개발팀의 능력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240106/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,27 @@
+import sys
+arr=list(map(int,input().split()))
+num1,num2,num3=0,0,0
+ans =0
+min_ans = sys.maxsize
+for i in range(5):
+    for j in range(i+1,5):
+        num1 = arr[i] + arr[j]
+        for k in range(5):
+            for p in range(k+1,5):
+                if i==k or i==p or j==k or j==p:
+                    continue
+                num2 = arr[k] + arr[p]
+                num3 = sum(arr) - num1 - num2
+                if num1 == num2 or num2 ==num3 or num1 == num3:
+                    continue
+                tmp=[]
+                tmp.append(num1)
+                tmp.append(num2)
+                tmp.append(num3)
+                tmp.sort()
+                ans = tmp[2] - tmp[0]
+                min_ans =min(ans,min_ans)
+            if len(tmp) == 0:
+                print(-1)
+                sys.exit()
+print(min_ans)
