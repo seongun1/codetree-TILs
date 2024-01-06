bd44fb2eae542bc6287fd65eb7a6f0f67dc3db53
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-06 20:50:27 +0900

    Title: 개발자의 능력 2, Time: 235ms, Memory: 24MB, Status: Passed - CodeTree

--- 240106/개발자의 능력 2/ability-of-developer-2.py
+++ 240106/개발자의 능력 2/ability-of-developer-2.py
@@ -1,10 +1,21 @@
 import sys
-arr=list(map(int,input().split()))
-n= len(arr)
-arr.sort()
-ans =[]
-for i in range(1,3):
-    tmp = arr[i] + arr[-i-1]
-    ans.append(tmp)
-
-print(max(ans) - min(ans))
+arr= list(map(int,input().split()))
+num1,num2,num3 = 0,0,0
+min_ans = sys.maxsize
+for i in range(6):
+    for j in range(i+1,6):
+        num1 = arr[i]+arr[j]
+        for k in range(6):
+            for p in range(k+1,6):
+                if i==k or i==p or j==k or j==p:
+                    continue
+                num2 =arr[k] + arr[p]
+                num3 = sum(arr) - num1 - num2
+                tmp=[]
+                tmp.append(num1)
+                tmp.append(num2)
+                tmp.append(num3)
+                tmp.sort()
+                ans = tmp[2] - tmp[0]
+                min_ans = min(ans,min_ans)
+print(min_ans)
