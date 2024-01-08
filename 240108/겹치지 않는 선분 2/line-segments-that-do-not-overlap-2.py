cd81da7323467f8b24a277b76c081095bdc5d4ae
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-08 18:43:42 +0900

    Title: 겹치지 않는 선분 2, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240108/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240108/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,18 +1,34 @@
 n=int(input())
 arr=[list(map(int,input().split())) for _ in range(n)]
+cnt = [True] * (n)
 
-cnt = [True] * (n+1)
-cnt[0] = False
 for i in range(n):
-    for j in range(i+1,n):
+    for j in range(n):
         x1,x2 = arr[i]
         x3,x4 = arr[j]
-        if x1 < x2 and x3 < x4:
+        if i==j:
+            continue
+        
+        if (x1 < x2 and x3 < x4) : #기울기 +,+
             if x3 < x1 < x2 < x4 or x1<x3 <x4 <x2:
                 cnt[i] = False
                 cnt[j] = False
+        elif (x1 > x2 and x3 >x4): #기울기 -,-
+            if x4 < x2 <= x3 <x1 or x2 < x4 <= x1 <x3:
+                cnt[i] =False
+                cnt[j] =False
+        elif(x1 < x2 and x3 > x4): #기울기 +,-
+            if x1 < x3 and x4 < x2:
+                cnt[i] = False
+                cnt[j] = False  
+        elif (x2 < x1 and x3 < x4): #기울기 -,+
+            if x1 > x3 and x2 <x4 :
+                cnt[i] =False
+                cnt[j] =False
+
 ans=0
-for t in cnt:
-    if t == True:
+for i in range(len(cnt)):
+    if cnt[i] == True:
+        print(i)
         ans +=1
 print(ans)
