d597fb73d810e81322d6721fe4ba62e84dd75cce
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-09 17:54:53 +0900

    Title: 상해버린 치즈, Time: 155ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240109/상해버린 치즈/rotten-cheese.py
+++ 240109/상해버린 치즈/rotten-cheese.py
@@ -6,13 +6,15 @@
 def find_cnt(arr):
     cnt =0
     for a in arr:
-        if a== True:
+        if a:
             cnt +=1
     return cnt
 
 n,m,d,s = map(int,input().split()) #n명이/ m가지의 치즈를 / d개의 먹은 기록 / 아팠는지 기록이 s
 arr_d = [list(map(int,input().split())) for _ in range(d)] #몇 번째 사람이 /  몇 번째 치즈를 / 언제 먹었는지
 arr_s = [list(map(int,input().split())) for _ in range (s)] #몇 번째 사람이 / 언제 아팠는지
+arr_d = sorted(arr_d,key =lambda x : x[2])
+arr_s = sorted(arr_s,key = lambda x : x[1])
 arr=[False] *(n+1) #아프면 True
 max_cnt =0
 tmp=[]
@@ -23,7 +25,7 @@ for i in range(s):
         if p_i == p_j and t_j < t_i: # 아프기 전의 먹은 치즈들(의심가는 치즈)을 다 tmp 배열에 넣는다.
             tmp.append([p_j,m_j,t_j])
 if s>1:
-    ans =[] # 겹치는 치즈를 구하는 배열
+    ans =[] # 교차검증
     for s in range(len(tmp)):
         a1,b1,c1 = tmp[s]
         for ss in range(len(tmp)):
@@ -35,12 +37,9 @@ if s>1:
                 break
 else:
     ans =tmp.copy()
-
-
-for t in range(len(ans)): #의심가는 치즈 중 하나만 상했을때를 가정 , 최대 약의 갯수를 구한다.
+for t in range(len(ans)): #의심가는 치즈 중 하나만 상했을때를 가정, 최대 약의 갯수를 구한다.
     a,b,c = ans[t] #만약 이것이 상했다면?
-    for k in range(d):
-        p,m,t = arr_d[k]
+    for p,m,t in arr_d:
         if b == m:
             arr[p] = True
     max_cnt = max(max_cnt,find_cnt(arr))
