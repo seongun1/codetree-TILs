2165c183f67e2cca2091ba6276926881dec9edd2
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-09 19:06:37 +0900

    Title: 선분 3개 지우기, Time: 151ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240109/선분 3개 지우기/remove-three-segments.py
@@ -0,0 +1,30 @@
+n = int(input())
+arr = [0] * (101)
+
+def check(arr):
+    for a in arr:
+        if a>=2:
+            return False
+    return True
+
+line = [list(map(int,input().split())) for _ in range(n)]
+cnt =0
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range (j+1,n):
+            flag = True
+            for paint in range(n):
+                if paint == i or paint == j or paint == k:
+                    continue
+                a,b = line[paint]
+                for s in range(a,b+1):
+                    arr[s] +=1
+            for a in arr:
+                if a>1:
+                    flag = False
+                    break
+            if flag:
+                cnt +=1
+            arr = [0] * (101)
+            
+print(cnt)
