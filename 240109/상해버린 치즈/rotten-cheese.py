a7825eeae2456544b661444a4a221fd1266db8b0
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-09 20:32:40 +0900

    Title: 상해버린 치즈, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240109/상해버린 치즈/rotten-cheese.py
+++ 240109/상해버린 치즈/rotten-cheese.py
@@ -1,48 +1,53 @@
-#n명이 m가지의 치즈를 먹었음. 정확히 하나가 상했다.
-#특정 사람이 어떤 치즈를 언제 먹었는지의 기록 d번
-#특정 사람이 언제 아팠는지에 대한 기록이 s번(기록된 것 외에 다른사람이 아플 수 있다.)
-#약이 최대 몇 개나 필요한가?
+N, M, D, S = tuple(map(int, input().split()))
+# (p번째 사람, m번째 치즈, t초) 먹음
+eaten = list(tuple(map(int, input().split())) for _ in range(D))
+# (p번째 사람, t초) 아픔
+bad_condition = list(tuple(map(int, input().split())) for _ in range(S))
 
-def find_cnt(arr):
-    cnt =0
-    for a in arr:
-        if a:
-            cnt +=1
-    return cnt
+# Step1 : 상한 치즈 고르기
+gone_cheezes = [[] for _ in range(N+1)]
 
-n,m,d,s = map(int,input().split()) #n명이/ m가지의 치즈를 / d개의 먹은 기록 / 아팠는지 기록이 s
-arr_d = [list(map(int,input().split())) for _ in range(d)] #몇 번째 사람이 /  몇 번째 치즈를 / 언제 먹었는지
-arr_s = [list(map(int,input().split())) for _ in range (s)] #몇 번째 사람이 / 언제 아팠는지
-arr_d = sorted(arr_d,key =lambda x : x[2])
-arr_s = sorted(arr_s,key = lambda x : x[1])
-arr=[False] *(n+1) #아프면 True
-max_cnt =0
-tmp=[]
-for i in range(s):
-    p_i,t_i = arr_s[i]
-    for j in range(d):
-        p_j,m_j,t_j = arr_d[j]
-        if p_i == p_j and t_j < t_i: # 아프기 전의 먹은 치즈들(의심가는 치즈)을 다 tmp 배열에 넣는다.
-            tmp.append([p_j,m_j,t_j])
-if s>1:
-    ans =[] # 교차검증
-    for s in range(len(tmp)):
-        a1,b1,c1 = tmp[s]
-        for ss in range(len(tmp)):
-            if s==ss:
-                continue
-            a2,b2,c2 = tmp[ss]
-            if b1 == b2:
-                ans.append([a1,b1,c1])
+bad_dict = {}
+for bad in bad_condition :
+    p, t = bad
+    bad_dict[p] = t
+
+eaten_dict = {}
+## 아프기 전 섭취했다면 ? 주워담는다.
+for eat in eaten :
+    p, m, t = eat
+    if eaten_dict.get(p):
+        eaten_dict[p].append((t, m))
+    else :
+        eaten_dict[p] = [(t, m)]
+
+for bad_p, bad_t in bad_dict.items()  :
+    # print(bad_p, bad_t)
+    # bad_t 이하로, bad_p의 치즈를 gone_cheezes[bad_p]에 주워담는다.
+    for eat_p, eat_cheezes in eaten_dict.items():
+        # print(eat_p, eat_cheezes)
+        for eat_t, eat_cheeze in eat_cheezes :
+            if eat_t < bad_t and eat_p == bad_p :
+                gone_cheezes[bad_p].append(eat_cheeze)
+
+### 주워담은 것들을 교집합 해서 상한 후보를 구한다.
+
+gone_cheezes = [i for i in gone_cheezes if i] # 유효값
+if gone_cheezes :
+    candi = list(set(gone_cheezes[0]).intersection(*gone_cheezes[1:]))
+
+
+# Step2 : 먹은 사람 세기
+max_cnt = 0
+for i in candi :
+    cnt = 0
+    for eat_p, eat_cheezes in eaten_dict.items():
+        for _, num in eat_cheezes :
+
+            if i == num :
+                cnt += 1
                 break
-else:
-    ans =tmp.copy()
-for t in range(len(ans)): #의심가는 치즈 중 하나만 상했을때를 가정, 최대 약의 갯수를 구한다.
-    a,b,c = ans[t] #만약 이것이 상했다면?
-    for p,m,t in arr_d:
-        if b == m:
-            arr[p] = True
-    max_cnt = max(max_cnt,find_cnt(arr))
-    arr = [False] *(n+1)
+                
+    max_cnt = max(max_cnt, cnt)
 
 print(max_cnt)
