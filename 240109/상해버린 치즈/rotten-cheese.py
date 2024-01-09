d17dba1c90458a45228094d83ccb04e172f6971a
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-09 20:42:00 +0900

    Title: 상해버린 치즈, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240109/상해버린 치즈/rotten-cheese.py
+++ 240109/상해버린 치즈/rotten-cheese.py
@@ -1,53 +1,30 @@
-N, M, D, S = tuple(map(int, input().split()))
-# (p번째 사람, m번째 치즈, t초) 먹음
-eaten = list(tuple(map(int, input().split())) for _ in range(D))
-# (p번째 사람, t초) 아픔
-bad_condition = list(tuple(map(int, input().split())) for _ in range(S))
+n,m,d,s = map(int,input().split())
 
-# Step1 : 상한 치즈 고르기
-gone_cheezes = [[] for _ in range(N+1)]
+arr1 = [list(map(int,input().split())) for _ in range(d)] #p,m,t
+arr2 = [list(map(int,input().split())) for _ in range(s)] #p,t
+max_cnt =0
+for i in range(1,m+1):
+    time = [0] *(n+1)
+    for a in arr1:
+        if a[1] != i:
+            continue
+        person = a[0]
+        if time[person] ==0:
+            time[person] = a[2]
+        elif time[person] > a[2]:
+            time[person] = a[2]
+    flag = True
+    for b in arr2:
+        person = b[0]
+        if time[person] ==0:
+            flag =False
+        if time[person] >= b[1]:
+            flag =False
+    cnt =0
+    if flag:
+        for j in range(1,n+1):
+            if time[j] != 0:
+                cnt +=1
 
-bad_dict = {}
-for bad in bad_condition :
-    p, t = bad
-    bad_dict[p] = t
-
-eaten_dict = {}
-## 아프기 전 섭취했다면 ? 주워담는다.
-for eat in eaten :
-    p, m, t = eat
-    if eaten_dict.get(p):
-        eaten_dict[p].append((t, m))
-    else :
-        eaten_dict[p] = [(t, m)]
-
-for bad_p, bad_t in bad_dict.items()  :
-    # print(bad_p, bad_t)
-    # bad_t 이하로, bad_p의 치즈를 gone_cheezes[bad_p]에 주워담는다.
-    for eat_p, eat_cheezes in eaten_dict.items():
-        # print(eat_p, eat_cheezes)
-        for eat_t, eat_cheeze in eat_cheezes :
-            if eat_t < bad_t and eat_p == bad_p :
-                gone_cheezes[bad_p].append(eat_cheeze)
-
-### 주워담은 것들을 교집합 해서 상한 후보를 구한다.
-
-gone_cheezes = [i for i in gone_cheezes if i] # 유효값
-if gone_cheezes :
-    candi = list(set(gone_cheezes[0]).intersection(*gone_cheezes[1:]))
-
-
-# Step2 : 먹은 사람 세기
-max_cnt = 0
-for i in candi :
-    cnt = 0
-    for eat_p, eat_cheezes in eaten_dict.items():
-        for _, num in eat_cheezes :
-
-            if i == num :
-                cnt += 1
-                break
-                
-    max_cnt = max(max_cnt, cnt)
-
+    max_cnt = max(cnt,max_cnt)
 print(max_cnt)
