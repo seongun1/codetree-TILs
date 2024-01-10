df0a6d3b6dcd6879e1db3a3024222f46af84b91a
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-10 19:09:41 +0900

    Title: 흥미로운 숫자 2, Time: 355ms, Memory: 73MB, Status: Passed - CodeTree

--- 240110/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240110/흥미로운 숫자 2/interesting-numbers-2.py
@@ -6,8 +6,16 @@ for i in range(x,y+1):
     tmp=[]
     for j in i:
         tmp.append(int(j))
-    tmp = set(tmp)
-    if len(tmp) == 2:
-        cnt +=1
 
+    dict_arr ={}
+    for t in tmp:
+        if t in dict_arr:
+            dict_arr[t] +=1
+        else:
+            dict_arr[t] =1
+    arr =list(dict_arr.values())
+    arr.sort()
+    if len(arr) == 2 and (arr[1] == len(tmp) -1 and arr[0] == 1):
+        cnt +=1    
+    
 print(cnt)
