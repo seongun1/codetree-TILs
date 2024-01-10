e995ce079fc332efc58841d7efe1f97554fc2287
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-10 19:57:00 +0900

    Title: 정보에 따른 숫자 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240110/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -0,0 +1,23 @@
+t,a,b =map(int,input().split())
+#d1이 d2보다 같거나 작은 경우 x= k는 특별한 위치
+s=[0] * (b+1)
+n=[0] * (b+1)
+for _ in range(t):
+    char , idx = input().split()
+    idx = int(idx)
+    if char =='S':
+        s[idx] = 1
+    elif char == 'N':
+        n[idx] = 1
+cnt =0
+for i in range(a,b+1):
+    d1,d2 = 1000,1000
+    for p in range(len(s)):
+        if s[p] == 1 and d1 > abs(i - p):
+            d1 =abs(i-p)
+    for k in range(len(n)):
+        if n[k] ==1 and d2 > abs(i - k):
+            d2 = abs(i-k)
+    if d1 <= d2:
+        cnt +=1
+print(cnt)
