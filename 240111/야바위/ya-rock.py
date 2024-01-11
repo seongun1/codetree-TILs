48008bc09ce7a09bb5b03e7670760acb81b69299
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-11 19:50:04 +0900

    Title: 야바위, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/야바위/ya-rock.py
+++ 240111/야바위/ya-rock.py
@@ -5,9 +5,9 @@ tmp = [list(map(int,input().split()))for _ in range(n)]
 for i in range(1,4):
     arr[i] = 1
     cnt=0
-    for t in range(n):
-        arr[tmp[t][0]],arr[tmp[t][1]] = arr[tmp[t][1]],arr[tmp[t][0]]
-        if arr[tmp[t][2]] == 1:
+    for a,b,c in tmp:
+        arr[a],arr[b] = arr[b],arr[a]
+        if arr[c] ==1:
             cnt +=1
     arr=[0] *4
     max_cnt = max(max_cnt,cnt)
