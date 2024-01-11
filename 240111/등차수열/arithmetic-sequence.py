d6f71a31dae5eef6bb9fb976c4f1de6a766c6e71
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-11 17:56:46 +0900

    Title: 등차수열, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240111/등차수열/arithmetic-sequence.py
@@ -0,0 +1,12 @@
+n=int(input())
+arr=list(map(int,input().split()))
+arr.sort()
+max_cnt =0
+for i in range(1,2001):
+    cnt =0
+    for j in range(n):
+        for k in range(j+1,n):
+            if i - arr[j] == arr[k] - i:
+                cnt +=1
+    max_cnt = max(cnt,max_cnt)
+print(max_cnt)
