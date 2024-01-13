0ffb2eab0a013d72c8c99bab3d48d8c41c736833
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-13 23:48:08 +0900

    Title: 독서실의 거리두기 5, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240113/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240113/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,22 +1,17 @@
-from sys import stdin
-n = int(stdin.readline())
-base = stdin.readline().strip()
+n=int(input())
+tmp = str(input())
+tmp = list(tmp)
+arr= list(map(int,tmp))
 
 ans = 0
-for i in range(n): #위치 선정
-    if base[i] == '1': #사용자 있음
-        continue
-    tmp = float("inf")
-    
-    indices = [
-        j for j in range(n)
-        if base[j] == '1' or i == j
-    ]
-
-    m = len(indices)
-
-    for i in range(m - 1):
-        tmp = min(tmp, indices[i + 1] - indices[i])
-
-    ans = max(ans, tmp)
+for i in range(n):
+    if arr[i] == 0:
+        arr[i] = 1
+        dist = n
+        for j in range(n):
+            for k in range(j+1,n):
+                if arr[j] == 1 and arr[k] ==1:
+                    dist = min(k-j,dist)
+        ans = max(dist,ans)
+        arr[i] = 0
 print(ans)
