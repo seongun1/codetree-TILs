909ebfa5ea525cef897a0bb7561566c666d9220d
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-15 18:47:07 +0900

    Title: 숫자들의 최대 차, Time: 191ms, Memory: 25MB, Status: Passed - CodeTree

--- 240115/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240115/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -1,19 +1,10 @@
 n,k = map(int,input().split())
 arr=[int(input()) for _ in range(n)]
-arr.sort()
-max_ans_len = 0
-for i in range(n):
-    start = arr[i]
-    ans =[]
-    ans.append(start)
-    for j in range(n):
-        if i==j:
-            continue
-        if min(ans) <= arr[j] <= max(ans):
-            ans.append(arr[j])
-        elif arr[j] < min(ans) and max(ans) - arr[j] <=k :
-            ans.append(arr[j])
-        elif max(ans) < arr[j] and arr[j] - min(ans) <= k:
-            ans.append(arr[j])
-    max_ans_len = max(len(ans),max_ans_len)
-print(max_ans_len)
+max_cnt =0
+for i in range(1,10001):
+    cnt =0
+    for a in arr:
+        if i <= a <= i+k:
+            cnt +=1
+    max_cnt = max(cnt,max_cnt)
+print(max_cnt)
