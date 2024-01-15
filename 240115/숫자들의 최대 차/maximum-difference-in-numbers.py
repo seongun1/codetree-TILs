ce3013b6dc721487f12f1e4f776aefff19d26bb3
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-15 18:43:53 +0900

    Title: 숫자들의 최대 차, Time: 270ms, Memory: 25MB, Status: Passed - CodeTree

--- 240115/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 240115/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -1,21 +1,30 @@
-n,k = map(int,input().split())
-arr=[int(input()) for _ in range(n)]
-arr.sort()
-max_ans_len = 0
-for i in range(n):
-    start = arr[i]
-    ans =[]
-    ans.append(start)
-    for j in range(n):
-        if i==j:
-            continue
-        if min(ans) <= arr[j] <= max(ans):
-            ans.append(arr[j])
-        elif arr[j] < min(ans):
-            if max(ans) - arr[j] <=k:
-                ans.append(arr[j])
-        elif max(ans) < arr[j]:
-            if arr[j] - min(ans) <= k:
-                ans.append(arr[j])
-    max_ans_len = max(len(ans),max_ans_len)
-print(max_ans_len)
+MAX_NUM = 10000
+
+# 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+arr = [
+    int(input())
+    for _ in range(n)
+]
+
+# 구간 [l, r] 
+# 사이에 들어있는 숫자 개수를 반환합니다.
+def count_num(l, r):
+    cnt = 0
+    for elem in arr:
+        if l <= elem and elem <= r:
+            cnt += 1
+
+    return cnt
+
+
+ans = 0
+# 크기가 K인 모든 구간을 잡아
+# 해당 구간 안에 들어오는 숫자의 개수를 세서
+# 그 중 최댓값을 계산합니다.
+for i in range(1, MAX_NUM + 1):
+    # 구간 [i, i + k] 사이에 들어있는 숫자를 세어
+    # 최댓값을 계산합니다.
+    ans = max(ans, count_num(i, i + k))
+
+print(ans)
