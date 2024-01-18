133b4d74358407c3b670fa825f2f6166c9456865
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-18 18:23:07 +0900

    Title: 이상한 폭탄 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240118/이상한 폭탄 3/strange-bomb-3.py
+++ 240118/이상한 폭탄 3/strange-bomb-3.py
@@ -2,7 +2,7 @@ n,k = map(int,input().split())
 arr=[int(input()) for _ in range(n)]
 
 tmp = [0 for i in range(n+1)]
-existed = [False for _ in range(n)]
+existed = [False for _ in range(n+1)]
 
 for i in range(n):
     bomb = arr[i]
