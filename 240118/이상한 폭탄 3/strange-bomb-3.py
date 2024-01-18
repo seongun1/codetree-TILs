40b4be82db52a52baa7c3a074ea7c437bd221117
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-18 18:18:31 +0900

    Title: 이상한 폭탄 3, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240118/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,22 @@
+n,k = map(int,input().split())
+arr=[int(input()) for _ in range(n)]
+
+tmp = [0 for i in range(n+1)]
+existed = [False for _ in range(n)]
+
+for i in range(n):
+    bomb = arr[i]
+    for j in range(i+1,n):
+        if bomb == arr[j] and j-i <=k:
+            existed[j] = True
+            existed[i] = True
+for k in range(n):
+    if existed[k] == True:
+        tmp[arr[k]] +=1
+ans =0
+count =0
+for index,bomb_count in enumerate(tmp):
+    if count < bomb_count:
+        ans = index
+        count = bomb_count
+print(ans)
