c0c5d82aef71294ce6e2f6b63442ecab8f5455be
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-22 19:42:35 +0900

    Title: 구역 청소, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240122/구역 청소/district-cleaning.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split())
+c,d = map(int,input().split())
+
+def is_overlap(a,b,c,d):
+    if b<c or d<a: #겹치지 않았음
+        return False
+    return True #겹쳤음
+
+if is_overlap(a,b,c,d):
+    print (abs(b-c))
+else:
+    print((b-a) + (d-c))
