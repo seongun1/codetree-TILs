c040b459cdc0130ad69a12e42495fcd837321f5a
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-25 22:00:32 +0900

    Title: 그래프 탐색, Time: 211ms, Memory: 27MB, Status: Passed - CodeTree

--- 240125/그래프 탐색/graph-traversal.py
+++ 240125/그래프 탐색/graph-traversal.py
@@ -6,21 +6,21 @@ for i in range(m):
     s,e = map(int,input().split())
     arr.append((s,e))
 
-# start = [i[0] for i in arr]
-# end = [i[1] for i in arr]
+start = [i[0] for i in arr]
+end = [i[1] for i in arr]
 
 visited = [False for _ in range(n+1)]
 
 graph = [[] for _ in range(n+1)]
 
 #  양 방향
-# for s,e in zip(start, end):
-#     graph[s].append(e)
-#     graph[e].append(s)
-
-for s,e in arr:
+for s,e in zip(start, end):
     graph[s].append(e)
     graph[e].append(s)
+
+# for s,e in arr:
+#     graph[s].append(e)
+#     graph[e].append(s)
 
 cnt = 0
 def dfs(vertex):
