32929fa991cb4eb57e9ce5b2cb1f6468d0ddc320
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-25 16:38:36 +0900

    Title: 순 간 이 동, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240125/순 간 이 동/teleportation.py
+++ 240125/순 간 이 동/teleportation.py
@@ -1,13 +1,11 @@
 a,b,x,y = map(int,input().split())
 
 dist = 0
-if abs(b - a) > abs(a-x) or b-a > abs(a-y):
+if abs(b - a) > abs(a-x) + abs(b-y) or abs(b-a) > abs(a-y) + abs(b-x):
     if abs(a-x) > abs(a-y):
-        dist += abs(a-y)
-        dist += abs(b-x)
+        dist += abs(a-y) + abs(b-x)
     else:
-        dist += abs(a-x)
-        dist += abs(b-y)
+        dist += abs(a-x) +abs(b-y)
 else:
     dist += abs(b-a)
 print(dist)
