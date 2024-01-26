ad3db30c1da7be8fc5d012973ea716f6494f1e08
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-26 17:14:07 +0900

    Title: ABC 줄 세우기, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240126/ABC 줄 세우기/abc-line-up.py
+++ 240126/ABC 줄 세우기/abc-line-up.py
@@ -9,9 +9,10 @@ while (1):
         if arr[i] != i:
             flag =False
             cnt +=1
-            for j in range(n):
-                if arr[i] == j:
-                    arr[i],arr[j] = arr[j],arr[i]
+            if abs(i-arr[i]) > abs(i+1 - arr[i]):
+                arr[i],arr[i+1] = arr[i+1],arr[i]
+            else:
+                arr[i],arr[i-1] < arr[i-1],arr[i]
     if flag:
         break
 print(cnt)
