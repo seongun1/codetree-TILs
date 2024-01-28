3abee9d51eb11cf6bbab93824ab8f443d5595e3d
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-28 12:01:58 +0900

    Title: n개의 점 중 m개 고르기, Time: 316ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240128/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240128/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -19,20 +19,20 @@ def choose(idx, cnt):
 
     if cnt == m:
         max_val = -sys.maxsize
-        for i, val in enumerate(ans):
-            for j, val1 in enumerate(ans):
+        for i, val1 in enumerate(ans):
+            for j, val2 in enumerate(ans):
                 if i != j:
-                    max_val = max(max_val, getDist(val, val1))
+                    max_val = max(max_val, getDist(val1, val2))
+
         min_val = min(min_val, max_val)
         return
-    
     if idx == n:
         return
 
     ans.append(arr[cnt])
-    choose(idx+1, cnt + 1)
+    choose(idx+1,cnt+1)
     ans.pop()
-    choose(idx+1, cnt)
+    choose(idx+1,cnt)
 
 choose(0,0)
 print(min_val)
