7eeb54ebbc3782813bd36835400f3a0c30aa7131
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-28 11:26:28 +0900

    Title: 단순한 동전 챙기기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240128/단순한 동전 챙기기/collect-coins-easy.py
@@ -0,0 +1,63 @@
+import sys
+
+min_val = sys.maxsize
+
+n = int(input())
+
+ans = list()
+arr = [
+    list(input())
+    for _ in range(n)
+]
+
+s,e = (-1,-1),(-1,-1)
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 'S':
+            s = (i,j)
+        elif arr[i][j] == 'E':
+            e = (i,j)
+
+tmp = list()
+for num in range(1,10):
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] == str(num):
+                tmp.append((i,j))
+
+def getDist(a,b):
+    x1, y1 = a
+    x2, y2 = b
+    return abs(x2-x1) + abs(y2-y1)
+
+def totalDist():
+    dist = getDist(s, ans[0])
+    dist += getDist(ans[0], ans[1])
+    dist += getDist(ans[1], ans[2])
+    dist += getDist(ans[2], e)
+    return dist
+
+visited = [False for _ in range(len(tmp))]
+
+def choose(cnt):
+    global min_val
+
+    if cnt == 3:
+        min_val = min(min_val, totalDist())
+        return
+
+    # if idx == len(tmp):
+    #     return
+    
+    for i in range(len(tmp)):
+        if not visited[i]:
+            visited[i] = True
+            ans.append(tmp[i])
+
+            choose(cnt+1)
+
+            ans.pop()
+            visited[i] = False
+choose(0)
+print(min_val)
