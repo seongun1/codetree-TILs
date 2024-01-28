1d4382ceac29b4db511131004f1c18bfed9b2975
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-28 11:57:15 +0900

    Title: n개의 점 중 m개 고르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240128/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240128/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -25,13 +25,14 @@ def choose(idx, cnt):
                 if i < j:
                     max_val = max(max_val, getDist(val, val1))
         min_val = min(min_val, max_val)
-
+    
     if idx == n:
         return
-    
+
     ans.append(arr[idx])
     choose(idx+1, cnt + 1)
     ans.pop()
     choose(idx+1, cnt)
+
 choose(0,0)
 print(min_val)
