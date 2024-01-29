2b87445c0afd89d1f2e1730f4ee414d5a6aa423d
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-30 00:05:22 +0900

    Title: 최소 경로로 탈출 하기, Time: 196ms, Memory: 29MB, Status: Passed - CodeTree

--- 240130/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 240130/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -17,12 +17,8 @@ visited = [
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
 
-def can_go(x,y):
-    if not in_range(x,y):
-        return False
-    if visited[x][y] or arr[x][y] == 0:
-        return False
-    return True
+def can_go(x, y):
+    return in_range(x, y) and arr[x][y] and visited[x][y] == 0
 
 x,y = 0,0
 q = deque()
