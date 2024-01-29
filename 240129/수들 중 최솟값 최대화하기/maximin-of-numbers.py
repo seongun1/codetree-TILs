9868a93dc17b96e29f43351e5b6100d05699131d
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-29 15:32:04 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 273ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240129/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,35 @@
+import sys
+
+n = int(input())
+ans = []
+max_val = -sys.maxsize
+
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+visited = [False for _ in range(n)]
+
+def calc():
+    return min(ans)
+
+def choose(cnt):
+    global max_val
+
+    if cnt == n:
+        max_val = max(max_val, calc())
+        return
+    
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            
+            ans.append(arr[i][cnt])
+            choose(cnt+1)
+            ans.pop()
+            
+            visited[i] = False
+
+choose(0)
+print(max_val)
