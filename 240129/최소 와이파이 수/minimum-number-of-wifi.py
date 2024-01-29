3839a9ccab04b0222e4cd7be927b489213134c8c
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-29 20:09:11 +0900

    Title: 최소 와이파이 수, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240129/최소 와이파이 수/minimum-number-of-wifi.py
@@ -4,21 +4,22 @@ arr= list(map(int,input().split()))
 cnt =0
 point =0
 visited=[False] * n
+
 while(1):
     if arr[point] == 1:
         #print(cnt)
         cnt +=1
         visited[point] = True
-        for i in range(point+1,n):
+        for i in range(point,n):
             if i-point <=2*m:
                 visited[i] = True
                 continue
             elif i-point>2*m:
-                point = i
+                point = i -1
                 break
     else:
-        point +=1
-    #print(visited,point)
+        visited[point] = True
     if visited[-1]:
         break
+    point +=1
 print(cnt)
