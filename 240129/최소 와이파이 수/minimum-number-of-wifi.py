75b32a8f25822951e26c1789fdf4d64d4cf3cc78
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-29 19:56:48 +0900

    Title: 최소 와이파이 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240129/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240129/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,17 +1,24 @@
 n,m = map(int,input().split())
 arr= list(map(int,input().split()))
+
 cnt =0
 point =0
+visited=[False] * n
 while(1):
-    if arr[point] ==1:
+    if arr[point] == 1:
+        #print(cnt)
+        cnt +=1
+        visited[point] = True
         for i in range(point+1,n):
-            if arr[i] == 1 and i - point <=m:
+            if i-point <=2*m:
+                visited[i] = True
                 continue
-            elif arr[i] ==1 and i - point >m:
-                cnt +=1
+            elif i-point>2*m:
                 point = i
+                break
     else:
         point +=1
-    if point == n-1:
+    #print(visited,point)
+    if visited[-1]:
         break
 print(cnt)
