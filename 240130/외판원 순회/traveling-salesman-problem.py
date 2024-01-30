32b9c44fd2cfaf03029badcff955d4552d28d8ad
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-30 18:15:03 +0900

    Title: 외판원 순회, Time: 344ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240130/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,47 @@
+import sys
+n = int(input())
+
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+visited = [False for _ in range(n)]
+ans = list()
+
+min_val = sys.maxsize
+
+def choose(cnt):
+    global min_val
+
+    if cnt == n:
+        tot = 0
+        for i in range(n-1):
+            cost = arr[ans[i]][ans[i+1]]
+
+            if cost == 0:
+                return
+            tot += cost
+        
+        addition = arr[ans[-1]][0]
+        if addition == 0:
+            return
+        
+        min_val = min(min_val, tot + addition)
+        return
+    
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            ans.append(i)
+
+            choose(cnt + 1)
+
+            ans.pop()
+            visited[i] = False
+
+
+ans.append(0)
+visited[0] = True
+choose(1)
+print(min_val)
