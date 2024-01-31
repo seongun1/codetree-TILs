e6347d44645fc1ca27bdfcc458b22795de6b0cb9
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-31 23:10:51 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240131/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240131/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -14,21 +14,21 @@ def min_init():
     d[0][0] = arr[0][0]
 
     for i in range(1,n):
-        d[0][i] = min(d[0][i-1], arr[0][i])
-        d[i][0] = min(d[i-1][0], arr[i][0])
+        d[0][i] = max(d[0][i-1], arr[0][i])
+        d[i][0] = max(d[i-1][0], arr[i][0])
 
 def max_init():
     d[0][0] = arr[0][0]
 
     for i in range(1,n):
-        d[0][i] = max(d[0][i-1], arr[0][i])
-        d[i][0] = max(d[i-1][0], arr[i][0])
+        d[0][i] = min(d[0][i-1], arr[0][i])
+        d[i][0] = min(d[i-1][0], arr[i][0])
 
 min_init()
 
 for i in range(1,n):
     for j in range(1,n):
-        d[i][j] = min(max(d[i-1][j], d[i][j-1]), arr[i][j])
+        d[i][j] = max(min(d[i-1][j], d[i][j-1]), arr[i][j])
 
 min_val = d[n-1][n-1]
 
@@ -38,9 +38,10 @@ d = [
 ]
 
 max_init()
+
 for i in range(1,n):
     for j in range(1,n):
-        d[i][j] = max(min(d[i-1][j], d[i][j-1]), arr[i][j])
+        d[i][j] = min(max(d[i-1][j], d[i][j-1]), arr[i][j])
 
 max_val = d[n-1][n-1]
 
