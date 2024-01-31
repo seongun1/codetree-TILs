478357401114c95c5eaaa8bbb3042672c01e52cd
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-31 21:25:21 +0900

    Title: 홀수 짝수의 묶음, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/홀수 짝수의 묶음/odd-even-bundle.py
+++ 240131/홀수 짝수의 묶음/odd-even-bundle.py
@@ -11,24 +11,32 @@ for i in arr:
 group = 0
 while True:
 
+# 짝수
     if group % 2 == 0:
-        # 짝수
+        
         if e > 0:
             e -= 1
             group += 1
+
         elif o >= 2:
             o -= 2
             group += 1
+
         else:
             if o > 0:
                 group -= 1
             break
+# 홀수 
     else:
+
         if o > 0:
             o -= 1
             group += 1
+
+        # elif e > 0:
+        #     # 홀수를 만들때는 ( 다수의 짝수 ) + 홀수 = 홀수가 되니까 이렇게 된다.
+        #     group -= 1
         else:
-            if e > 0:
-                group -= 1
             break
+
 print(group)
