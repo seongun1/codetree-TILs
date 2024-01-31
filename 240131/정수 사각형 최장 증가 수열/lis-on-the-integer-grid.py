696161f32cef77ef08a920c75442fb76fe3f79f7
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-31 23:21:56 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 899ms, Memory: 56MB, Status: Passed - CodeTree

--- 240131/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240131/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -5,52 +5,34 @@ arr = [
     for _ in range(n)
 ]
 
-max_cnt = 0
-
 dxs,dys = [-1,1,0,0], [0,0,-1,1]
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
 
-def dfs(x,y):
-    global cnt
-    tmp = 0
+d = [
+    [1 for _ in range(n)]
+    for _ in range(n)
+]
+
+cells = []
+for i in range(n):
+    for j in range(n):
+        cells.append((arr[i][j], i, j))
+
+# 정수값이 큰 쪽으로 이동 -> 작은 값부터 for loop를 채워야 한다
+cells.sort()
+
+max_val = 0
+for _,x,y in cells:
+
     for dx, dy in zip(dxs, dys):
-        next_x, next_y = x + dx , y + dy
+        next_x, next_y = x + dx, y + dy
 
-        if not in_range(next_x, next_y):
-            cnt.append(tmp)
-            tmp = 0
-            continue
-        
-        if arr[x][y] >= arr[next_x][next_y]:
-            cnt.append(tmp)
-            tmp = 0
-            continue
-        
-        # if visited[next_x][next_y]:
-        #     continue
+        if in_range(next_x, next_y) and arr[next_x][next_y] > arr[x][y]:
+            d[next_x][next_y] = max(d[next_x][next_y], d[x][y] + 1)
 
-        #print("arr[x][y] : {}, arr[next_x][next_y] : {}".format(arr[x][y], arr[next_x][next_y]))
-        tmp += 1
-        #visited[next_x][next_y] = True
-        dfs(next_x, next_y)
-    cnt.append(tmp)
-
-for x in range(n):
-    for y in range(n):
-        cnt = []
-        
-        # visited = [
-        #     [False for _ in range(n)]
-        #     for _ in range(n)
-        # ]
-
-        # visited[x][y] = True
-        dfs(x,y)
-
-        #print(x,y,cnt)
-
-        max_cnt = max(max_cnt,max(cnt))
-
-print(max_cnt+1)
+for i in range(n):
+    for j in range(n):
+        max_val = max(max_val, d[i][j])
+print(max_val)
