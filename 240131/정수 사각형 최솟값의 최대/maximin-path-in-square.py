5fe07ede91fb973704e9afe4c0bf614bf10d11e6
김성운 <115334493+seongun1@users.noreply.github.com>
2024-01-31 21:32:17 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240131/정수 사각형 최솟값의 최대/maximin-path-in-square.py
+++ 240131/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -14,13 +14,14 @@ def init():
     d[0][0] = arr[0][0]
 
     for i in range(1,n):
-        d[0][i] = min(d[0][i-1] , arr[0][i])
+        d[0][i] = min(d[0][i-1], arr[0][i])
     for i in range(1,n):
         d[i][0] = min(d[i-1][0], arr[i][0])
 
 init()
+
 for i in range(1,n):
     for j in range(1,n):
-        d[i][j] = min(max(d[i-1][j], d[i][j-1]) , arr[i][j])
+        d[i][j] = min(max(d[i-1][j], d[i][j-1]), arr[i][j])
 
 print(d[n-1][n-1])
