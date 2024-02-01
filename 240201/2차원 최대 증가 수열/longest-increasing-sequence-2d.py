7477ecbb1dcc5e80bf5bf68ee6e82e5a6ac4ffe9
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-01 11:06:00 +0900

    Title: 2차원 최대 증가 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240201/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240201/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -1,4 +1,8 @@
+import sys
+
 n,m = map(int,input().split())
+
+INT_MIN = -sys.maxsize
 
 d = [
     [ 1 for _ in range(m)]
@@ -10,12 +14,26 @@ arr = [
     for _ in range(n)
 ]
 
+def initialize():
+    for i in range(n):
+        for j in range(n):
+            d[i][j] = INT_MIN
+    d[0][0] = 1
+
+initialize()
+
 for i in range(1,n):
     for j in range(1,m):
         for x in range(i):
             for y in range(j):
+                if d[x][y] == INT_MIN:
+                    continue
+
+
                 if arr[i][j] > arr[x][y]:
                     d[i][j] = max(d[i][j], d[x][y] + 1)
+                else:
+                    continue
 
 ans = 0
 for i in range(n):
