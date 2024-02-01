816b4950e2656ab1984f2e44c6b7bb47770047fa
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-01 10:32:44 +0900

    Title: 최대 점프 횟수, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240201/최대 점프 횟수/maximum-number-of-jumps.py
+++ 240201/최대 점프 횟수/maximum-number-of-jumps.py
@@ -3,18 +3,21 @@ import sys
 n = int(input())
 arr = list(map(int,input().split()))
 
-d = [0 for _ in range(n+1)] 
+d = [0] *n
 
 def init():
-    for i in range(1,n+1):
+    for i in range(n):
         d[i] = -sys.maxsize
+    d[0] = 0
+
+init()
 
 for i in range(1,n):
     for j in range(i):
-        if d[i] == -sys.maxsize:
-            continue
+        # if d[i] == -sys.maxsize:
+        #     continue
         
         if j + arr[j] >= i:
             d[i] = max(d[i], d[j] + 1)
-
+#print(d)
 print(max(d))
