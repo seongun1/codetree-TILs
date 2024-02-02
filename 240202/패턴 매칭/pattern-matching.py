402ee472170d27f21c582177625441bbabb8f34f
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-02 10:43:20 +0900

    Title: 패턴 매칭, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240202/패턴 매칭/pattern-matching.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline()
+
+s = input
+p = input
+
+n = len(s)
+m = len(p)
+
+d = [
+    [0 for _ in range(m+1)]
+    for _ in range(n+1)
+]
+
+def init():
+    d[0][0] = 0
+
+    for i in range(1,n+1):
+        d[i][0] = i
+    for i in range(1,m+1):
+        d[0][i] = i
+
+init()
+
+for i in range(1,n+1):
+    flag = False
+    for j in range(1,m+1):
+        if s[i-1] == p[i-1]:
+            break
+        if '*' == p[i-1]:
+            sys.exit(0)
+        if '.' == p[i-1]:
+            break
+        else:
+            flag = True
+            break
+    if flag:
+        break
+    
+if flag:
+    print("false")
+else:
+    print("true")
