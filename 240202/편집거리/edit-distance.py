70825be4cb8710ee5ecf9bb4c619a95d05221fea
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-02 10:16:36 +0900

    Title: 편집거리, Time: 209ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240202/편집거리/edit-distance.py
@@ -0,0 +1,33 @@
+a = input()
+b = input()
+
+n = len(a)
+m = len(b)
+
+d = [
+    [0 for _ in range(m+1)]
+    for _ in range(n+1)
+]
+
+def init():
+    
+    for i in range(1,n+1):
+        d[i][0] = i
+        
+    for i in range(1,m+1):
+        d[0][i] = i
+        
+
+init()
+#print(d)
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if a[i-1] == b[j-1]:
+            d[i][j] = d[i-1][j-1]
+        else:
+            d[i][j] = min(d[i-1][j-1], d[i-1][j], d[i][j-1]) + 1
+        #print("i,j,d[i][j]",i,j,d[i][j],"<- ", d[i-1][j-1])
+
+#print(d)
+    
+print(d[n][m])
