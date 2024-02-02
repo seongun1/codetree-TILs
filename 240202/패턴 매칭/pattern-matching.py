0b7418162a3ba9fd97bbc59c526eb34021cdbab7
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-02 19:59:02 +0900

    Title: 패턴 매칭, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/패턴 매칭/pattern-matching.py
+++ 240202/패턴 매칭/pattern-matching.py
@@ -1,41 +1,54 @@
-# import sys
+import sys
 
-# s = input()
-# p = input()
+a = input()
+b = input()
 
-# n = len(s)
-# m = len(p)
+n = len(a)
+m = len(b)
+
+d = [
+    [False for _ in range(m+1)]
+    for _ in range(n+1)
+]
+
+a = " " + a
+b = " " + b
+
+d[0][0] = True
+
+for j in range(m):
+    for i in range(n):
+        if not d[i][j]:
+            continue
+        
+        if j != m-1 and b[j+2] == '*':
+            d[i][j+2] = True
+
+            for cur_i in range(i+1, n+1):
+                if b[j+1] != '.' and a[cur_i] != b[j+1]:
+                    break
+                d[cur_i][j+2] = True
+        
+        elif b[j+1] == ".":
+            d[i+1][j+1] = True
+        elif b[j+1] == a[i+1]:
+            d[i+1][j+1] = True
+
+print("true" if d[n][m] else "false")
 
 
-# def possible():
-#     for i in range(1,n+1):
-#         if (s[i-1] == p[i-1]) or (p[i-1] == '.'):
-#             continue
-#         elif (p[i-1] == '*'):
-#             for j in range(i,n+1):
-#                 if s[i-1] == s[j-1]:
-#                     return True
-                
-#         else:
-#             return False
-#     return True
+# import re
 
-# if possible():
+# def is_match(s, p):
+#     pattern = re.compile(p)
+#     return bool(pattern.fullmatch(s))
+
+# # 입력 받기
+# s = input().strip()
+# p = input().strip()
+
+# # 결과 출력
+# if is_match(s, p):
 #     print("true")
 # else:
 #     print("false")
-import re
-
-def is_match(s, p):
-    pattern = re.compile(p)
-    return bool(pattern.fullmatch(s))
-
-# 입력 받기
-s = input().strip()
-p = input().strip()
-
-# 결과 출력
-if is_match(s, p):
-    print("true")
-else:
-    print("false")
