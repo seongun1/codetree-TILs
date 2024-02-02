1f0ea3e7fd89064ccf28d38654319b179079b852
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-02 11:09:22 +0900

    Title: 패턴 매칭, Time: 302ms, Memory: 25MB, Status: Passed - CodeTree

--- 240202/패턴 매칭/pattern-matching.py
+++ 240202/패턴 매칭/pattern-matching.py
@@ -1,24 +1,41 @@
-import sys
+# import sys
 
-s = input()
-p = input()
+# s = input()
+# p = input()
 
-n = len(s)
-m = len(p)
+# n = len(s)
+# m = len(p)
 
 
-def possible():
-    for i in range(1,n+1):
-        #print("s[i-1] : {}, p[i-1]] : {}".format(s[i-1], p[i-1]))
-        if (s[i-1] == p[i-1]) or (p[i-1] == '.'):
-            continue
-        elif (p[i-1] == '*') :
-            return True
-        else:
-            return False
-    return True
+# def possible():
+#     for i in range(1,n+1):
+#         if (s[i-1] == p[i-1]) or (p[i-1] == '.'):
+#             continue
+#         elif (p[i-1] == '*'):
+#             for j in range(i,n+1):
+#                 if s[i-1] == s[j-1]:
+#                     return True
+                
+#         else:
+#             return False
+#     return True
 
-if possible():
+# if possible():
+#     print("true")
+# else:
+#     print("false")
+import re
+
+def is_match(s, p):
+    pattern = re.compile(p)
+    return bool(pattern.fullmatch(s))
+
+# 입력 받기
+s = input().strip()
+p = input().strip()
+
+# 결과 출력
+if is_match(s, p):
     print("true")
 else:
     print("false")
