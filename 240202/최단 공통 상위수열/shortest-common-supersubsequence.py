fa67d22e5ba953f8418bb3533de08d1141998c91
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-02 21:00:22 +0900

    Title: 최단 공통 상위수열, Time: 187ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240202/최단 공통 상위수열/shortest-common-supersubsequence.py
+++ 240202/최단 공통 상위수열/shortest-common-supersubsequence.py
@@ -8,22 +8,29 @@ d = [
     for _ in range(n+1)
 ]
 
-def init():
-    for i in range(1,n+1):
-        d[i][0] = i
+# def init():
+#     d[0][0] = m * n
+#     for i in range(1,n+1):
+#         d[i][0] = 
         
-    for i in range(1,m+1):
-        d[0][i] = i
+#     for i in range(1,m+1):
+#         d[0][i] = i
 
-init()
+# init()
 
+#print(d)
 for i in range(1,n+1):
     for j in range(1,m+1):
-        #print("s[i] : {}, t[j] : {}".format(s[i-1], t[j-1]))
         if s[i-1] == t[j-1]:
             d[i][j] = d[i-1][j-1] - 1
-            #print("d[i][j] : {}, d[i-1][j-1] : {}".format(d[i][j],d[i-1][j-1]))
         # else:
         #     d[i][j] = max(d[i-1][j], d[i][j-1])
+
+
 #print(d)
-print(d[n][m])
+ans  = n * m
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if d[i][j] < ans:
+            ans = d[i][j]
+print(ans)
