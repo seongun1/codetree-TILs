dea250e033719e12d18fc99dc30be9d51b1ba7dc
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-14 17:40:50 +0900

    Title: 양수 직사각형의 최대 크기, Time: 310ms, Memory: 29MB, Status: Passed - CodeTree

--- 240214/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240214/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,6 +1,5 @@
-import sys
-
 n,m = map(int,input().split())
+
 arr = [
     list(map(int,input().split()))
     for _ in range(n)
@@ -26,13 +25,12 @@ for i in range(n):
             for w in range(1,m+1):
                 if not can_go(i,j,h,w):
                     continue
-                
-                sum_val = 0
 
+                sum_val = 0
+                
                 for x in range(i,i+h):
                     for y in range(j,j+w):
                         sum_val += 1
                 
-
-                ans = max(sum_val, ans)
+                ans = max(ans, sum_val)
 print(ans)
