01af80055ebedf70a8b9b9f942128c3716365f0c
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-14 16:07:01 +0900

    Title: 금 채굴하기, Time: 356ms, Memory: 26MB, Status: Passed - CodeTree

--- 240214/금 채굴하기/gold-mining.py
+++ 240214/금 채굴하기/gold-mining.py
@@ -10,14 +10,13 @@ def getCost(k):
     return k ** 2 + (k+1) ** 2
 
 def getGold(x,y):
-    max_gold = -sys.maxsize
-    for k in range(n):
+    max_gold = 0
+    for k in range(2 * (n-1) + 1):
         tmp = 0
         for i in range(n):
             for j in range(n):
                 if abs(x-i) + abs(y-j) <= k:
-                    if arr[i][j]:
-                        tmp += 1
+                    tmp += arr[i][j]
 
         if tmp * m >= getCost(k):
             max_gold = max(max_gold, tmp)
