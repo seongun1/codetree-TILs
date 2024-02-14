2e65d94cda796c8b70901b7dbee8d950d861bc85
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-14 17:27:41 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 474ms, Memory: 29MB, Status: Passed - CodeTree

--- 240214/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240214/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -1,19 +1,18 @@
 import sys
 
 n,m = map(int,input().split())
+
 arr = [
     list(map(int,input().split()))
     for _ in range(n)
 ]
-
-ans = -sys.maxsize
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
 
 visited = [[False] * m for _ in range(n)]
 
-def able(x,y,h,w):
+def can_go(x,y,h,w):
     for i in range(x,x+h):
         for j in range(y,y+w):
             if not in_range(i,j):
@@ -22,7 +21,7 @@ def able(x,y,h,w):
                 return False
     return True
 
-def make():
+def otherBox():
     max_val = -sys.maxsize
     for i in range(n):
         for j in range(m):
@@ -32,32 +31,34 @@ def make():
             for h in range(1,n+1):
                 for w in range(1,m+1):
                     sum_val = 0
-                    if not able(i,j,h,w):
+
+                    if not can_go(i,j,h,w):
                         continue
-                    
+
                     for x in range(i,i+h):
                         for y in range(j,j+w):
                             sum_val += arr[x][y]
-                
+                    
                     max_val = max(max_val, sum_val)
     return max_val
 
+ans = -sys.maxsize
 for i in range(n):
     for j in range(m):
         for h in range(1,n+1):
             for w in range(1,m+1):
+
+                if not can_go(i,j,h,w):
+                    continue
                 
                 sum_val = 0
 
-                if not able(i,j,h,w):
-                    continue
-                
                 for x in range(i,i+h):
                     for y in range(j,j+w):
                         sum_val += arr[x][y]
                         visited[x][y] = True
 
-                ans = max(ans, sum_val + make())
+                ans = max(ans, sum_val + otherBox())
 
                 for x in range(i,i+h):
                     for y in range(j,j+w):
