4f6e00816575588eb0d56df13c574a3b26838f3f
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-16 15:47:09 +0900

    Title: 선택 정렬 구현, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240216/선택 정렬 구현/implement-selection-sort.py
@@ -0,0 +1,9 @@
+n=int(input())
+arr = list(map(int,input().split()))
+
+for i in range(n):
+    for j in range(i+1,n):
+        if arr[i] > arr[j]:
+            arr[i],arr[j] = arr[j],arr[i]
+for a in arr:
+    print(a,end=' ')
