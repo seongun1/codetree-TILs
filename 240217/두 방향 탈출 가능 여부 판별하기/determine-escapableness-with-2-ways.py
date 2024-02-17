bc8ac1d19445b07b847c7f1b77668309d8086a58
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-17 19:20:25 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 160ms, Memory: 27MB, Status: Passed - CodeTree

--- 240217/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240217/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -5,30 +5,30 @@ arr = [
     for _ in range(n)
 ]
 
-def in_range(x,y):
-    return 0<=x<n and 0<=y<m
-
 visited = [
     [0 for _ in range(m)]
     for _ in range(n)
 ]
 
+def in_range(x,y):
+    return 0<=x<n and 0<=y<m
+
 def can_go(x,y):
     if not in_range(x,y):
         return False
-
     if visited[x][y] or arr[x][y] == 0:
         return False
     return True
 
+dxs,dys = [1,0],[0,1]
+
 def dfs(x,y):
-    if x == n-1 and y == m-1:
+
+    if x == n - 1 and y == m - 1:
         return
-
-    dxs,dys = [1,0],[0,1]
-
+    
     for dx, dy in zip(dxs, dys):
-        nx, ny = x + dx , y + dy
+        nx ,ny = x+ dx , y + dy
 
         if can_go(nx,ny):
             visited[nx][ny] += 1
@@ -36,5 +36,4 @@ def dfs(x,y):
 
 visited[0][0] = 1
 dfs(0,0)
-
 print(visited[n-1][m-1])
