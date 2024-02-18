53386a7372689efc457139cf9316ec494b32d8ea
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-18 16:03:13 +0900

    Title: 안전 지대, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240218/안전 지대/comfort-zone.py
+++ 240218/안전 지대/comfort-zone.py
@@ -5,10 +5,10 @@ arr = [
     for _ in range(n)
 ]
 
-# visited = [
-#     [False for _ in range(m)]
-#     for _ in range(n)
-# ]
+visited = [
+    [False for _ in range(m)]
+    for _ in range(n)
+]
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
