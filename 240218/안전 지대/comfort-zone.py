2d6999ad0b044e266cd2f8256af735f914f2cdb0
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-18 16:03:08 +0900

    Title: 안전 지대, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240218/안전 지대/comfort-zone.py
@@ -0,0 +1,53 @@
+n,m = map(int,input().split())
+
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+# visited = [
+#     [False for _ in range(m)]
+#     for _ in range(n)
+# ]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<m
+
+def can_go(x,y,k):
+    if not in_range(x,y):
+        return False
+    if visited[x][y] or arr[x][y] <= k:
+        return False
+    return True
+
+dxs,dys = [1,-1,0,0],[0,0,-1,1]
+
+def dfs(x,y,k):
+
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+
+        if can_go(nx, ny ,k):
+            visited[nx][ny] = True
+            dfs(nx,ny,k)
+    
+
+ans = list()
+for k in range(1,101):
+    cnt = 0
+
+    visited = [
+        [False for _ in range(m)]
+        for _ in range(n)
+    ]
+
+    for i in range(n):
+        for j in range(m):
+            if can_go(i,j,k):
+                cnt += 1
+                visited[i][j] = True
+                dfs(i,j,k)
+
+    ans.append((cnt, k))
+ans.sort(key = lambda x : (-x[0]))
+print(ans[0][0], ans[0][1])
