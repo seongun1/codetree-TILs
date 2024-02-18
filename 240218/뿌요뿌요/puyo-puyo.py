d7c75bb4a3ce74fcb5e1fac372a3b4d4ec31de99
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-18 16:25:23 +0900

    Title: 뿌요뿌요, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240218/뿌요뿌요/puyo-puyo.py
@@ -0,0 +1,51 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+n = int(input())
+arr = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [False for _ in range(n)]
+    for _ in range(n)
+]
+
+dxs, dys = [-1,1,0,0], [0,0,-1,1]
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def can_go(x,y,num):
+    if not in_range(x,y):
+        return False
+    if visited[x][y] or arr[x][y] != num:
+        return False
+    return True
+
+def dfs(x,y,num):
+    global cnt
+
+    for dx, dy in zip(dxs, dys):
+        nx,ny = x + dx, y + dy
+
+        if can_go(nx, ny, num):
+            visited[nx][ny] = True
+            cnt += 1
+            dfs(nx,ny,num)
+
+
+res = []
+for i in range(n):
+    for j in range(n):
+        num = arr[i][j]
+        if can_go(i,j,num):
+            cnt = 1
+            visited[i][j] = True
+            dfs(i,j,num)
+        
+            if cnt >= 4:
+                res.append(cnt)
+
+print(len(res), max(res))
