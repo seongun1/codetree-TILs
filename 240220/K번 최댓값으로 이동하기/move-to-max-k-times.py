2510f6c093d5e0b99a1e2cd2ae0ba93e91985194
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-20 16:14:09 +0900

    Title: K번 최댓값으로 이동하기, Time: 173ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240220/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 240220/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -8,7 +8,6 @@ arr = [
 ]
 
 r,c = map(int,input().split())
-
 r -= 1
 c -= 1
 
@@ -24,50 +23,46 @@ def can_go(x,y,val):
         return False
     return True
 
-dxs, dys = [-1,1,0,0], [0,0,-1,1]
+dxs,dys = [-1,1,0,0], [0,0,-1,1]
 
-def bfs(value):
+def bfs(val):
     global max_val, max_pos
 
     while q:
         x,y = q.popleft()
 
         for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
+            nx,ny = x + dx, y + dy
 
-            if can_go(nx,ny, value):
+            if can_go(nx,ny, val):
                 visited[nx][ny] = True
                 q.append((nx,ny))
                 max_val = max(max_val, arr[nx][ny])
     
     for i in range(n):
         for j in range(n):
-            if visited[i][j] and arr[i][j] == max_val:
+            if arr[i][j] == max_val:
                 max_pos.append((i,j))
-            
-
 
 for _ in range(k):
-
     q = deque()
 
-    max_val, max_pos = 0, []
+    max_val, max_pos = 0,list()
 
     visited = [
         [False for _ in range(n)]
         for _ in range(n)
     ]
 
-    visited[r][c] = True
     q.append((r,c))
+    visited[r][c] = True
     bfs(arr[r][c])
 
     if not max_pos:
         break
-
+    
     max_pos.sort(key = lambda x : (x[0], x[1]))
-
     r,c = max_pos[0]
-    ans_x, ans_y = max_pos[0] 
-        
+    ans_x,ans_y = max_pos[0]
+
 print(ans_x + 1, ans_y + 1)
