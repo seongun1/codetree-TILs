da7c6bd15db745798cd5af0b573b8d740f3b49c5
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-23 18:47:19 +0900

    Title: 뱀은 사과를 좋아해, Time: 5089ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240223/뱀은 사과를 좋아해/snake-loves-apples.py
+++ 240223/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -1,30 +1,30 @@
+from collections import deque
 n,m,k = map(int,input().split())
 
-arr = [[0] * n for _ in range(n)]
+arr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
 
-# 사과 = 2
+# 사과 있는 장소
 for _ in range(m):
     x,y = map(int,input().split())
     arr[x-1][y-1] = 2
 
-
-direct = []
-total_cnt = 0 # 뱀이 움직이는 총 횟수
+# 방향 , 얼마나 움직일거야?
+turn = []
+whole = 0
 for i in range(k):
     d,length = input().split()
-    total_cnt += int(length)
-    direct.append([d, int(length)])
+    turn.append([d, int(length)])
+    whole += int(length)
 
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
 
-def meet_my_body(x,y):
-    if arr[x][y] == 1: # 만남
-        return True
-    return False
+# 뱀의 위치
+x,y = 0,0
 
-
-# 동 남 서 북 -> 오른쪽을 보니까 시계 방향 반시계 방향 체크 
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 
@@ -35,48 +35,45 @@ mapper = {
     'U': 3
 }
 
-x,y = 0,0
-
-def process():
-    global x, y, total_cnt
+def move():
+    global x, y, whole
     time = 0
     arr[x][y] = 1 # 뱀 위치
     q = [(x,y)]
-
-    if k == 0:
-        return 0
-
+    
     while True:
+        
+        for direction, p in turn:
 
-        for d,length in direct:
-
-            if total_cnt == 0:
+            if whole == 0:
                 return time
 
-            for j in range(length):
+            for k in range(p):
+                nx,ny = x + dx[mapper[direction]], y + dy[mapper[direction]]
 
-                nx, ny = x + dx[mapper[d]], y + dy[mapper[d]]
-                
                 time += 1
 
                 if not in_range(nx,ny):
                     return time
 
-                if arr[nx][ny] == 2: # 사과 있음 
+                if arr[nx][ny] == 2:
                     arr[nx][ny] = 1
                     q.append((nx,ny))
-                
-                elif arr[nx][ny] == 0 or arr[nx][ny] == 1: # 사과 없음
+
+                elif arr[nx][ny] == 0 or arr[nx][ny]:
+                    
                     hx,hy = q.pop(0)
                     arr[hx][hy] = 0
-                    if meet_my_body(nx,ny):
+
+                    if arr[nx][ny]:
                         return time
+
                     arr[nx][ny] = 1
                     q.append((nx,ny))
                 
+                
                 x,y = nx,ny
-                total_cnt -= 1
+                whole -= 1
 
-
-ans = process()
+ans = move()
 print(ans)
