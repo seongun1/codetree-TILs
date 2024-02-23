30a8dd3dca7bd3ef78b0fde306b3c4f6a07d848d
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-23 17:35:42 +0900

    Title: 대폭발, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240223/대폭발/big-explosion.py
@@ -0,0 +1,42 @@
+n,m,x,y = map(int,input().split())
+
+x -= 1
+y -= 1
+
+arr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+arr[x][y] = 1
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+dxs, dys = [-1,1,0,0], [0,0,-1,1]
+
+for t in range(m):
+    dist = 2 ** t
+    new_arr = [
+        [0 for _ in range(n)]
+        for _ in range(n)
+    ]
+    
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j]:
+                for dx,dy in zip(dxs, dys):
+                    nx ,ny = x + dx, y + dy
+
+                    new_arr[nx][ny] = 1
+
+    for i in range(n):
+        for j in range(n):
+            if new_arr[i][j]:
+                arr[i][j] = 1
+    
+val = 0
+for i in range(n):
+    for j in range(n):
+        val += arr[i][j]
+print(val)
