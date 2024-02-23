d69994b27b9f7f706da8774096bf50b7f792d133
김성운 <115334493+seongun1@users.noreply.github.com>
2024-02-23 17:53:26 +0900

    Title: 대폭발, Time: 141ms, Memory: 26MB, Status: Passed - CodeTree

--- 240223/대폭발/big-explosion.py
+++ 240223/대폭발/big-explosion.py
@@ -15,32 +15,36 @@ def in_range(x,y):
 
 dxs, dys = [-1,1,0,0], [0,0,-1,1]
 
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
 for t in range(m):
     dist = 2 ** t
-    
+
     new_arr = [
         [0 for _ in range(n)]
         for _ in range(n)
     ]
-    
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j]:
+
+    for x in range(n):
+        for y in range(n):
+            if arr[x][y]:
                 for dx,dy in zip(dxs, dys):
-                    nx ,ny = i + dx * dist, j + dy * dist
+                    nx,ny = x+dx * dist, y+dy * dist
 
                     if not in_range(nx,ny):
                         continue
-
+ 
                     new_arr[nx][ny] = 1
-
-    for i in range(n):
-        for j in range(n):
-            if new_arr[i][j]:
-                arr[i][j] = 1
     
+    for x in range(n):
+        for y in range(n):
+            if new_arr[x][y]:
+                arr[x][y] = 1
+
 val = 0
 for i in range(n):
     for j in range(n):
-        val += arr[i][j]
+        if arr[i][j]:
+            val += 1
 print(val)
