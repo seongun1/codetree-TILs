a0ad34a649ffe737fcde3c4eec3d1d28bd555fec
김성운 <115334493+seongun1@users.noreply.github.com>
2024-03-04 21:56:50 +0900

    Title: 1차원 윷놀이, Time: 555ms, Memory: 29MB, Status: Passed - CodeTree

--- 240304/1차원 윷놀이/yutnori-1d.py
+++ 240304/1차원 윷놀이/yutnori-1d.py
@@ -3,21 +3,22 @@ import sys
 n,m,k = map(int,input().split())
 arr = list(map(int,input().split()))
 
-max_val = -sys.maxsize
+max_val = 0
 ans = [1] * k
 
 def calc():
+    global max_val
     cnt = 0
     for i in ans:
         if i >= m:
             cnt += 1
-    return cnt
+    max_val = max(max_val, cnt)
 
 def choose(idx):
     global max_val
 
     if idx == n:
-        max_val = max(max_val, calc())
+        calc()
         return
 
     for i in range(k):
@@ -28,6 +29,7 @@ def choose(idx):
         choose(idx+1)
         ans[i] -= arr[idx]
 
+    calc()
     return
 
 choose(0)
