d7688d9a16cebbe4fa84bf196f9e18894b7bf135
김성운 <115334493+seongun1@users.noreply.github.com>
2024-03-29 18:58:14 +0900

    Title: 괄호 쌍의 개수, Time: 5083ms, Memory: 33MB, Status: Time Limit Exceed - CodeTree

---
+++ 240329/괄호 쌍의 개수/number-of-parenthesis-pairs.py
@@ -0,0 +1,39 @@
+arr =input()
+
+arr=list(arr)
+flag = False
+ans = 0
+
+def is_correct():
+    cnt =0
+    stack = arr.copy()
+    cnt_a,cnt_b = 0,0
+
+    while (stack):
+        tmp = stack.pop()
+        if tmp =='(':
+            cnt_a +=1
+            cnt -=1
+        else:
+            cnt_b +=1
+            cnt +=1
+        if cnt <0:
+            return False
+    if cnt_a != cnt_b:
+        return False
+    return True
+
+for i in range(len(arr)):
+    if arr[i] == '(':
+        arr[i] = ')'
+    else: #a == ')'
+        arr[i]='('
+    #print(arr)
+    if is_correct():
+        #print(i)
+        ans +=1
+    if arr[i] == '(':
+        arr[i] = ')'
+    else:
+        arr[i] = '('
+print(ans)
