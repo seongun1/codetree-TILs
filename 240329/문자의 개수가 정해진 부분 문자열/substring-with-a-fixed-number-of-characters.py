b9502bb155752f6c3614132ffa19b3842a86bbc2
김성운 <115334493+seongun1@users.noreply.github.com>
2024-03-29 18:26:17 +0900

    Title: 문자의 개수가 정해진 부분 문자열, Time: 5029ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240329/문자의 개수가 정해진 부분 문자열/substring-with-a-fixed-number-of-characters.py
+++ 240329/문자의 개수가 정해진 부분 문자열/substring-with-a-fixed-number-of-characters.py
@@ -1,19 +1,29 @@
 n,a,b = map(int,input().split())
 arr= input()
 arr = list(arr)
+
 max_cnt = 0
-index = 0
+left = 0
+
 cnt_a,cnt_b = 0,0
-for index in range(0,n):
-    for right in range(n,-1,-1):
-        tmp = arr[index:right]
+
+def check(a,b,cnt_a,cnt_b):
+    return cnt_a <= a and cnt_b>=b
+
+for left in range(0,n):
+    for right in range(left+1,n+1):
+        tmp = arr[left:right]
+        #print(tmp)
         for t in tmp:
             if t == 'a':
                 cnt_a +=1
             else:
                 cnt_b+=1
-        if (cnt_a <=a and cnt_b >= b and max_cnt < len(tmp)):
-            max_cnt = len(tmp)
+        if check(a,b,cnt_a,cnt_b):
+            max_cnt = max(len(tmp),max_cnt)
+        elif cnt_a > a:
+            cnt_a,cnt_b = 0,0 
+            break
         cnt_a,cnt_b = 0,0 
 
 print(max_cnt)
