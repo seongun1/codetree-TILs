f0a513518d59b7df7471cbc005bafa60316c3a19
김성운 <115334493+seongun1@users.noreply.github.com>
2024-04-01 23:31:36 +0900

    Title: 문자의 개수가 정해진 부분 문자열, Time: 203ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240401/문자의 개수가 정해진 부분 문자열/substring-with-a-fixed-number-of-characters.py
+++ 240401/문자의 개수가 정해진 부분 문자열/substring-with-a-fixed-number-of-characters.py
@@ -6,28 +6,27 @@ arr = list(arr)
 max_cnt = 0
 left = 0
 
-cnt_a,cnt_b = 0,0
+global cnt_a
+global cnt_b
 
 def check(a,b,cnt_a,cnt_b):
     return cnt_a <= a and cnt_b>=b
 
+def count_alpha(tmp):
+    cnt_a,cnt_b = 0,0
+    for i in range(len(tmp)):
+        if tmp[i] =='a':
+            cnt_a +=1
+        else:
+            cnt_b +=1
+        if cnt_a > a:
+            tmp = tmp[:i]
+            return tmp
+    return tmp
+
 for left in range(0,n):
-    for right in range(n,0,-1):
-        tmp = arr[left:right]
-        #print(tmp)
-        for t in tmp:
-            if t == 'a':
-                cnt_a +=1
-            else:
-                cnt_b+=1
-        if check(a,b,cnt_a,cnt_b):
-            max_cnt = max(len(tmp),max_cnt)
-            #print(tmp)
-            cnt_a,cnt_b = 0,0 
-            break
-        elif cnt_b < b:
-            cnt_a,cnt_b = 0,0 
-            break
-        cnt_a,cnt_b = 0,0 
+    tmp = arr[left:len(arr)]
+    tmp = count_alpha(tmp)
+    max_cnt = max(max_cnt,len(tmp))
 
 print(max_cnt)
