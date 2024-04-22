89f79bc9757f28b60acdd34dd50c3b5e9329bbcc
김성운 <115334493+seongun1@users.noreply.github.com>
2024-04-22 16:18:33 +0900

    Title: 겹치지 않게 선분 고르기, Time: 157ms, Memory: 24MB, Status: Passed - CodeTree

--- 240422/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240422/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -7,10 +7,12 @@ for i in range(n):
 
 line.sort(key=lambda x:x[1])
 
-cnt = 0
-for i in range(n):
-    for j in range(i+1,n):
-        if line[i][1] < line[j][0]:
-            cnt += 1
-            break
-print(cnt+1)
+cnt = 1
+idx = line[0][1]
+
+for i in range(1,n):
+    if idx < line[i][0]:
+        cnt += 1
+        idx = line[i][1]
+
+print(cnt)
