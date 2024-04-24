d01ddf865f4d57292e3fd6cb92834180041b4838
김성운 <115334493+seongun1@users.noreply.github.com>
2024-04-24 12:02:25 +0900

    Title: 알파벳과 사칙연산, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240424/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,46 @@
+string = input()
+
+alpha,oper,ans=[],[],[]
+max_val = 0
+
+for i in string:
+    if i.isalpha():
+        alpha.append(i)
+    else:
+        oper.append(i)
+
+dic = {}
+for i in alpha:
+    if i not in dic:
+        dic[i] = 0
+
+def calc():
+    for i,k in enumerate(dic.keys()):
+        dic[k] = ans[i] 
+
+    val = dic[alpha[0]]
+    for i, op in enumerate(oper):
+        next_val = dic[alpha[i+1]]
+        if op == '+':
+            val += next_val
+        if op == '-':
+            val -= next_val
+        if op == '*':
+            val *= next_val
+    return val
+
+
+def choose(cnt):
+    global max_val
+
+    if cnt == len(alpha):
+        max_val = max(max_val, calc())
+        return 
+
+    for i in range(1,5):
+        ans.append(i)
+        choose(cnt+1)
+        ans.pop()
+
+choose(0)
+print(max_val)
