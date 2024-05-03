4f3cec5178f7b4e7ea15808dd0948bc67de15f4c
김성운 <115334493+seongun1@users.noreply.github.com>
2024-05-03 23:53:21 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
+++ 240503/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -23,20 +23,20 @@ dys = [0,0,1,1,1,0,-1,-1,-1]
 def in_range(x,y):
     return 0<=x<n and 0<=y<n
 
-def can_go(x,y, prev):
+def can_go(x,y,prev):
     return in_range(x,y) and arr[x][y] > prev
 
-def choose(x,y, cnt):
+def choose(x,y,cnt):
     global max_val
 
-    max_val = max(max_val, cnt)
+    max_val = max(max_val,cnt)
 
     d = direction_arr[x][y]
 
     for i in range(n):
         nx,ny = x + dxs[d] * i, y + dys[d] * i
-        if can_go(nx,ny,arr[x][y]):
-            choose(nx,ny,cnt+1)
 
+        if can_go(nx,ny, arr[x][y]):
+            choose(nx,ny,cnt+1)
 choose(x,y,0)
 print(max_val)
