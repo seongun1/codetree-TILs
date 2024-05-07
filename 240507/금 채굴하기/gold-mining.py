bbee1f9586e05a0446bec45bc4dc9b255b3907d3
김성운 <115334493+seongun1@users.noreply.github.com>
2024-05-07 11:32:56 +0900

    Title: 금 채굴하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240507/금 채굴하기/gold-mining.py
@@ -0,0 +1,24 @@
+n,m = map(int,input().split())
+
+
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+def count_gold():
+    max_ans = -1
+    for k in range(50):
+        if k>n:
+            break
+        for i in range(n):
+            for j in range(n):
+                gold = 0
+                #마름모 만들기
+                for rr in range(n):
+                    for cc in range(n):
+                        if arr[rr][cc] == 1 and abs(i-rr) + abs(j - cc) <= k:
+                            gold += 1
+                spend = k *k + (k+1) * (k+1)
+                profit = (gold*m) - spend
+                if profit > 0:
+                    max_ans = max(gold,max_ans)
+    return max_ans
+print(count_gold())
