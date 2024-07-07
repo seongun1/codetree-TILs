64660ae78f9b21708930f2ac8d00fe2556adea5a
김성운 <115334493+seongun1@users.noreply.github.com>
2024-07-07 17:46:40 +0900

    Title: 괄호 쌍의 개수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240707/괄호 쌍의 개수/number-of-parenthesis-pairs.py
+++ 240707/괄호 쌍의 개수/number-of-parenthesis-pairs.py
@@ -1,23 +1,63 @@
-tmp = input()
-tmp = list(tmp)
+def count_valid_transformations(s):
+    n = len(s)
+    
+    # 전체 균형 확인
+    total_open = s.count('(')
+    total_close = n - total_open
 
-def is_balanced_and_valid(s):
+    # 균형이 맞지 않으면 절대 올바른 괄호 문자열이 될 수 없음
+    if total_open != total_close:
+        return 0
+
+    # 접두사 균형 확인
+    prefix_balance = [0] * (n + 1)
     balance = 0
-    for char in s:
-        if char == '(':
+    for i in range(n):
+        if s[i] == '(':
             balance += 1
         else:
             balance -= 1
-        if balance < 0:
-            return False
-    return balance == 0
+        prefix_balance[i + 1] = balance
 
-cnt = 0
-for i in range(len(tmp)):
-    original = tmp[i]
-    tmp[i] = ')' if tmp[i] == '(' else '('  # Flip the bracket
-    if is_balanced_and_valid(tmp):
-        cnt += 1
-    tmp[i] = original  # Restore the original bracket
+    cnt = 0
+    for i in range(n):
+        if s[i] == '(':
+            # 변경: '(' -> ')'
+            new_balance = balance - 2
+        else:
+            # 변경: ')' -> '('
+            new_balance = balance + 2
 
-print(cnt)
+        # 새로운 전체 균형이 맞는지 확인
+        if new_balance != 0:
+            continue
+        
+        # 변경 후 올바른 접두사 균형 확인
+        valid = True
+        for j in range(i + 1):
+            if s[j] == '(':
+                new_prefix_balance = prefix_balance[j] - 1
+            else:
+                new_prefix_balance = prefix_balance[j] + 1
+            if new_prefix_balance < 0:
+                valid = False
+                break
+
+        if valid:
+            for j in range(i + 1, n):
+                if s[j] == '(':
+                    new_prefix_balance += 1
+                else:
+                    new_prefix_balance -= 1
+                if new_prefix_balance < 0:
+                    valid = False
+                    break
+
+        if valid:
+            cnt += 1
+
+    return cnt
+
+# 테스트 케이스 입력
+s = input()
+print(count_valid_transformations(s))
